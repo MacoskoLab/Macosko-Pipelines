3805ea01f581ea0e42f33ad5fd30c0591583dd21
MatthewShabet <matthew.shabet@gmail.com>
2024-06-09 21:14:56 -0400

    Update main.rs

--- variants/summary/main.rs
+++ variants/summary/main.rs
@@ -14,17 +14,28 @@ fn main() {
     }
     let file = hdf5::File::open(&args[1]).expect("Could not open reads.h5 input file");
     
-    // load the .h5 data
-    let data = load_data(&file); // Vec<DataEntry> (read, rname_i, mapq, cb_i, ub_i)
-    let order_map: HashMap<ReadNum, usize> = data.iter().map(|e| e.read).collect::<Vec<ReadNum>>().iter().enumerate().map(|(i, &num)| (num, i)).collect();
-    let snv = load_snv(&file, &order_map); // Vec<SNVEntry> (read, pos, alt, qual)
-    let matches = load_matches(&file, &order_map); // Vec<MatchEntry> (read, start, end)
+    // Load the .h5 data
+    let mut data = load_data(&file);       // Vec<DataEntry>  (read, rname_i, mapq, cb_i, ub_i)
+    let mut snv = load_snv(&file);         // Vec<SNVEntry>   (read, pos, alt, qual)
+    let mut ins = load_ins(&file);         // Vec<InsEntry>   (read, pos, str_i)
+    let mut del = load_del(&file);         // Vec<DelEntry>   (read, pos, len)
+    let mut matches = load_matches(&file); // Vec<MatchEntry> (read, start, end)
     
-    // Create data structures (umi) (cb_i) (ub_i) (reads) (avg_mapq)
+    // Sort the .h5 data
+    data.sort_by(|d1, d2| {(d1.cb_i).cmp(&d2.cb_i).then((d1.ub_i).cmp(&d2.ub_i))}); // Sort by cb, umi
+    let order_map: HashMap<ReadNum, usize> = data.iter().map(|e| e.read).collect::<Vec<ReadNum>>()
+                                                 .iter().enumerate().map(|(i, &num)| (num, i)).collect();
+    snv.sort_by_key(|e| order_map.get(&e.read).expect("E"));                // Sort by umi
+    matches.sort_by_key(|e| order_map.get(&e.read).expect("E"));            // Sort by umi
+    ins.sort_by_key(|e| order_map.get(&e.read).expect("E"));                // Sort by umi
+    del.sort_by_key(|e| order_map.get(&e.read).expect("E"));                // Sort by umi
+    
+    // Create data structures (umi) (cb_i) (ub_i) (rname_i) (reads) (avg_mapq)
     let mut data_umi: Vec<ReadNum> = Vec::new();
     let mut data_cb_i: Vec<WLi> = Vec::new();
     let mut data_ub_i: Vec<WLi> = Vec::new();
     let mut data_rname_i: Vec<RNAMEi> = Vec::new();
+    let mut data_strand: Vec<u8> = Vec::new();
     let mut data_reads: Vec<ReadNum> = Vec::new();
     let mut data_mapq_avg: Vec<u8> = Vec::new();
     // Create snv table structure (snv_i) (RNAME) (POS) (ALT)
@@ -35,6 +46,14 @@ fn main() {
     let mut snv_hq: Vec<ReadNum> = Vec::new();
     let mut snv_lq: Vec<ReadNum> = Vec::new();
     let mut snv_total: Vec<ReadNum> = Vec::new();
+    // Create ins structures (umi) (pos) (str_i)
+    //let mut ins_umi: Vec<ReadNum> = Vec::new();
+    //let mut ins_pos: Vec<SeqPos> = Vec::new();
+    //let mut ins_str_i: Vec<WLi> = Vec::new();
+    // Create del structures (umi) (pos) (len)
+    //let mut del_umi: Vec<ReadNum> = Vec::new();
+    //let mut del_pos: Vec<SeqPos> = Vec::new();
+    //let mut del_len: Vec<SeqPos> = Vec::new();
     // Create matches structures (umi) (start) (end)
     let mut match_umi: Vec<ReadNum> = Vec::new();
     let mut match_start: Vec<SeqPos> = Vec::new();
@@ -46,11 +65,10 @@ fn main() {
 
     // Loop through the reads
     let mut umi: ReadNum = 0;
-    let mut curr_cb: WLi = data[0].cb_i;
-    let mut curr_ub: WLi = data[0].ub_i;
-    let mut snv_idx = 0; let mut matches_idx = 0;
-    let mut umi_dict: HashMap<RNAMEi,UMIinfo> = HashMap::new(); // rname_i -> (reads, mapq, snv_i, start, end)
-    for d in data { // d is (read, rname_i, mapq, cb_i, ub_i)
+    let mut curr_cb: WLi = data[0].cb_i; let mut curr_ub: WLi = data[0].ub_i;
+    let mut snv_idx = 0; let mut ins_idx = 0; let mut del_idx = 0; let mut matches_idx = 0;
+    let mut umi_dict: HashMap<(RNAMEi,u8), UMIinfo> = HashMap::new(); // (rname_i,strand) -> (reads, mapq, snv_i, start, end)
+    for d in data { // d is (read, flag, rname_i, mapq, cb_i, ub_i)
         if (d.cb_i != curr_cb) || (d.ub_i != curr_ub) {
           // remove chimerism, take dominant RNAME
           let total_reads: ReadNum = umi_dict.values().map(|record| record.reads).sum();
@@ -58,12 +76,13 @@ fn main() {
           if umi_dict.values().filter(|record| record.reads == max_reads).count() > 1 { // if two RNAME tie for max read support, discard
               chimeric_reads += total_reads;
           } else { // get the RNAME with the most read support
-              let (&rname_i, umi_info) = umi_dict.iter().max_by_key(|entry| entry.1.reads).expect("E");
+              let (&(rname_i,strand), umi_info) = umi_dict.iter().max_by_key(|entry| entry.1.reads).expect("E");
               // write aggregate data
               data_umi.push(umi);
               data_cb_i.push(curr_cb); 
               data_ub_i.push(curr_ub);
               data_rname_i.push(rname_i);
+              data_strand.push(strand);
               data_reads.push(umi_info.reads);
               data_mapq_avg.push(average_round_u8(&umi_info.mapq));
               // write aggregate snv
@@ -82,14 +101,14 @@ fn main() {
                 match_end.push(interval.1);
               }
               // write metadata
-              chimeric_reads += total_reads - max_reads;
+              chimeric_reads += total_reads - umi_info.reads;
               umi += 1;
           }
           // reset umi struct
           umi_dict = HashMap::new();
           curr_cb = d.cb_i; curr_ub = d.ub_i;
         }
-        let umi_info: &mut UMIinfo = umi_dict.entry(d.rname_i).or_insert(UMIinfo::new());
+        let umi_info: &mut UMIinfo = umi_dict.entry((d.rname_i,flag2strand(d.flag))).or_insert(UMIinfo::new());
         umi_info.reads += 1;
         umi_info.mapq.push(d.mapq);
 
@@ -103,6 +122,20 @@ fn main() {
             snv_idx += 1;
         }
         
+        // collect ins data
+        while ins_idx<ins.len() && ins[ins_idx].read==d.read { // ins is (read, pos, str_i)
+            //umi_info.start.push(matches[matches_idx].start);
+            //umi_info.end.push(matches[matches_idx].end);
+            ins_idx += 1;
+        }
+        
+        // collect del data
+        while del_idx<del.len() && del[del_idx].read==d.read { // del is (read, pos, len)
+            //umi_info.start.push(matches[matches_idx].start);
+            //umi_info.end.push(matches[matches_idx].end);
+            del_idx += 1;
+        }
+        
         // collect matches data
         while matches_idx<matches.len() && matches[matches_idx].read==d.read { // matches is (read, start, end)
             umi_info.start.push(matches[matches_idx].start);
@@ -111,15 +144,9 @@ fn main() {
         }
     }
     assert!(snv_idx == snv.len());
+    assert!(ins_idx == ins.len());
+    assert!(del_idx == del.len());
     assert!(matches_idx == matches.len());
-    
-    // let ins_read: Vec<u64> = file.dataset("ins/read").expect("not found").read_raw().expect("E");
-    // let ins_pos: Vec<u64> = file.dataset("ins/pos").expect("not found").read_raw().expect("E"); assert!(ins_read.len() == ins_pos.len());
-    // let ins_str_i: Vec<u64> = file.dataset("ins/str_i").expect("not found").read_raw().expect("E"); assert!(ins_read.len() == ins_str_i.len());
-    
-    // let del_read: Vec<u64> = file.dataset("del/read").expect("not found").read_raw().expect("E");
-    // let del_pos: Vec<u64> = file.dataset("del/pos").expect("not found").read_raw().expect("E"); assert!(del_read.len() == del_pos.len());
-    // let del_len: Vec<u64> = file.dataset("del/len").expect("not found").read_raw().expect("E"); assert!(del_read.len() == del_len.len());
     
     println!("Saving output...");
     
@@ -132,6 +159,7 @@ fn main() {
     data_cb_i.write_vector(&data_group, "cb_i");         // cb whitelist index (0-indexed)
     data_ub_i.write_vector(&data_group, "ub_i");         // ub whitelist index (0-indexed)
     data_rname_i.write_vector(&data_group, "rname_i");   // rname whitelist index (0-indexed)
+    data_strand.write_vector(&data_group, "strand");     // 0 normal, 1 means reverse complemented
     data_reads.write_vector(&data_group, "reads");       // number of reads for the umi
     data_mapq_avg.write_vector(&data_group, "mapq_avg"); // integer average mapq for all reads of the umi
     // write the SNV table
@@ -149,7 +177,19 @@ fn main() {
     snv_snv_i.write_vector(&snv_group, "snv_i");     // SNV table index (0-indexed)
     snv_hq.write_vector(&snv_group, "hq");           // number of high-quality reads
     snv_lq.write_vector(&snv_group, "lq");           // number of low-quality reads
-    snv_total.write_vector(&snv_group, "total");     // number of total reads
+    snv_total.write_vector(&snv_group, "total");     // number of total reads observing its POS
+    // write the ins data
+    //let ins_group = file.create_group("ins").expect(".h5 error");
+    //assert_all_same(&[ins_umi.len(), ins_pos.len(), ins_str_i.len()]);
+    //ins_umi.write_vector(&ins_group, "umi");         // umi number (key)
+    //ins_pos.write_vector(&ins_group, "pos");         // 0-indexed position of reference base after insertion
+    //ins_str_i.write_vector(&ins_group, "str_i");     // index into whitelist/ins (0-indexed)
+    // write the del data
+    //let del_group = file.create_group("del").expect(".h5 error");
+    //assert_all_same(&[del_umi.len(), del_pos.len(), del_len.len()]);
+    //del_umi.write_vector(&del_group, "umi");         // umi number (key)
+    //del_pos.write_vector(&del_group, "pos");         // 0-indexed position of first deleted reference base
+    //del_len.write_vector(&del_group, "len");         // number of deleted bases
     // write the matches data
     let match_group = file.create_group("match").expect(".h5 error");
     assert_all_same(&[match_umi.len(), match_start.len(), match_end.len()]);
