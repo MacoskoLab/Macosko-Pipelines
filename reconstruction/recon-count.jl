d66e8b4e649a7084c499d3b72b61b79d3ae807f1
MatthewShabet <matthew.shabet@gmail.com>
2024-07-12 02:40:50 -0400

    Update recon-count.jl

--- reconstruction/recon-count.jl
+++ reconstruction/recon-count.jl
@@ -39,7 +39,16 @@ end
 @assert length(unique([fastq_seq_len(R1) for R1 in R1s])) == 1 "WARNING: R1s have different FASTQ sequence lengths - proceed only if you are sure they have the same read structure"
 @assert length(unique([fastq_seq_len(R2) for R2 in R2s])) == 1 "WARNING: R2s have different FASTQ sequence lengths - proceed only if you are sure they have the same read structure"
 
-# UP matching methods
+# UMI matching+compressing methods
+const px = [convert(UInt32, 4^i) for i in 0:(9-1)]
+function UMItoindex(UMI::StringView{SubArray{UInt8, 1, Vector{UInt8}, Tuple{UnitRange{Int64}}, true}})::UInt32
+    return(dot(px, (codeunits(UMI).>>1).&3))
+end
+const bases = ['A','C','T','G'] # MUST NOT change this order
+function indextoUMI(i::UInt32)::String15
+    return(String15(String([bases[(i>>n)&3+1] for n in 0:2:16])))
+end
+
 function listHDneighbors(str, hd, charlist = ['A','C','G','T','N'])::Set{String}
     res = Set{String}()
     for inds in combinations(1:length(str), hd)
@@ -56,24 +65,58 @@ function listHDneighbors(str, hd, charlist = ['A','C','G','T','N'])::Set{String}
     end
     return(res)
 end
-const UP1 = "TCTTCAGCGTTCCCGAGA"
-const UP2 = "CTGTTTCCTG"
-const UP1_whitelist = Set{String31}(reduce(union, [listHDneighbors(UP1, i) for i in 0:3]))
-const UP2_whitelist = Set{String15}(reduce(union, [listHDneighbors(UP2, i) for i in 0:2]))
 const umi_homopolymer_whitelist = Set{String15}(reduce(union, [listHDneighbors(str, i) for str in [c^9 for c in ["A","C","G","T"]] for i in 0:2]))
 
-# UMI compressing methods
-const px = [convert(UInt32, 4^i) for i in 0:(9-1)]
-function UMItoindex(UMI::StringView{SubArray{UInt8, 1, Vector{UInt8}, Tuple{UnitRange{Int64}}, true}})::UInt32
-    return(dot(px, (codeunits(UMI).>>1).&3))
+function get_R2_V9(record::FASTX.FASTQ.Record)
+    sb2_1 = FASTQ.sequence(record, 1:8)
+    up2 = FASTQ.sequence(record, 9:26)
+    sb2_2 = FASTQ.sequence(record, 27:33)
+    umi2 = FASTQ.sequence(record, 34:42)
+    return sb2_1, sb2_2, up2, umi2
 end
-const bases = ['A','C','T','G'] # MUST NOT change this order
-function indextoUMI(i::UInt32)::String15
-    return(String15(String([bases[(i>>n)&3+1] for n in 0:2:16])))
+function get_R2_V15(record::FASTX.FASTQ.Record)
+    sb2_1 = FASTQ.sequence(record, 1:8)
+    sb2_2 = FASTQ.sequence(record, 9:15)
+    up2 = FASTQ.sequence(record, 16:25)
+    umi2 = FASTQ.sequence(record, 26:34)
+    return sb2_1, sb2_2, up2, umi2
+end
+const get_R2 = Ref{Function}()
+
+# Determine the R2 bead type
+function learn_R2type(R2)
+    iter = R2 |> open |> GzipDecompressorStream |> FASTQ.Reader
+    UPseq9 = String31("TCTTCAGCGTTCCCGAGA")
+    UPseq15 = String15("CTGTTTCCTG")
+    s9 = 0 ; s15 = 0
+    for (i, record) in enumerate(iter)
+        i > 100000 ? break : nothing
+        s9  += FASTQ.sequence(record, 9:26)  == UPseq9
+        s15 += FASTQ.sequence(record, 16:25) == UPseq15
+    end
+    println("V9: ", s9, " V15: ", s15)
+    return(s9 >= s15 ? "V9" : "V15")
 end
 
 println("R1 bead type: V8/V10")
-println("R2 bead type: V15")
+const UP1 = "TCTTCAGCGTTCCCGAGA"
+const UP1_whitelist = Set{String31}(reduce(union, [listHDneighbors(UP1, i) for i in 0:3]))
+
+println("Learning the R2 bead type")
+res_list = [learn_R2type(R2) for R2 in R2s]
+if all(x -> x == "V9", res_list)
+    println("R2 bead type: V9")
+    const UP2 = "TCTTCAGCGTTCCCGAGA"
+    const UP2_whitelist = Set{String31}(reduce(union, [listHDneighbors(UP2, i) for i in 0:3]))
+    get_R2[] = get_R2_V9
+elseif all(x -> x == "V15", res_list)
+    println("R2 bead type: V15")
+    const UP2 = "CTGTTTCCTG"
+    const UP2_whitelist = Set{String15}(reduce(union, [listHDneighbors(UP2, i) for i in 0:2]))
+    get_R2[] = get_R2_V15
+else
+    error("Error: The R2 bead type is not consistent ($res_list)")
+end
 
 ####################################################################################################
 
@@ -110,10 +153,7 @@ function process_fastqs(R1s, R2s)
             sb1_2 = FASTQ.sequence(record[1], 27:33)
             umi1 = FASTQ.sequence(record[1], 34:42)
 
-            sb2_1 = FASTQ.sequence(record[2], 1:8)
-            sb2_2 = FASTQ.sequence(record[2], 9:15)
-            up2 = FASTQ.sequence(record[2], 16:25)
-            umi2 = FASTQ.sequence(record[2], 26:34)
+            sb2_1, sb2_2, up2, umi2 = get_R2[](record[2])
 
             skip = false
             if occursin('N', umi1)
@@ -310,15 +350,46 @@ println("done") ; flush(stdout) ; GC.gc()
 
 print("Writing Output... ") ; flush(stdout)
 
+# Normalize the barcode indexes
+m1 = sort(collect(Set(df.sb1_i)))
+m2 = sort(collect(Set(df.sb2_i)))
+dict1 = Dict{UInt32, UInt32}(value => index for (index, value) in enumerate(m1))
+dict2 = Dict{UInt32, UInt32}(value => index for (index, value) in enumerate(m2))
+sb1_whitelist_short = sb1_whitelist[m1]
+sb2_whitelist_short = sb2_whitelist[m2]
+sb1_new = [dict1[k] for k in df.sb1_i]
+sb2_new = [dict2[k] for k in df.sb2_i]
+@assert sb1_whitelist[df.sb1_i] == sb1_whitelist_short[sb1_new]
+@assert sb2_whitelist[df.sb2_i] == sb2_whitelist_short[sb2_new]
+df.sb1_i = sb1_new
+df.sb2_i = sb2_new
+
+# Create total counts of UMI per bead
+df1 = combine(groupby(df, :sb1_i), :umi => sum => :umi)
+sort!(df1, :sb1_i)
+@assert df1.sb1_i == collect(1:nrow(df1))
+df2 = combine(groupby(df, :sb2_i), :umi => sum => :umi)
+sort!(df2, :sb2_i)
+@assert df2.sb2_i == collect(1:nrow(df2))
+
+# Write UMI histograms
+p1 = histogram(log10.(df1[!, :umi]), yscale = :log10, bins = 100, legend = false,
+          xlabel = "log10 umi", ylabel = "log10 beads", title = "Histogram of total umi per sb1",
+          yticks = [10^i for i in 0:10], linewidth = 0, linealpha = 0, fillcolor = :gray50)
+p2 = histogram(log10.(df2[!, :umi]), yscale = :log10, bins = 100, legend = false,
+          xlabel = "log10 umi", ylabel = "log10 beads", title = "Histogram of total umi per sb2",
+          yticks = [10^i for i in 0:10], linewidth = 0, linealpha = 0, fillcolor = :gray50)
+p = plot(p1, p2, layout = (2, 1), size=(7*100, 8*100))
+savefig(p, joinpath(out_path, "histograms.pdf"))
+
 # Write the metadata
-s = string
 function f(num)
     num = string(num)
     num = reverse(join([reverse(num)[i:min(i+2, end)] for i in 1:3:length(num)], ","))
     return(num)
 end
 function d(num1, num2)
-    f(num1)*" ("*s(round(num1/num2*100, digits=2))*"%"*")"
+    f(num1)*" ("*string(round(num1/num2*100, digits=2))*"%"*")"
 end
 m = metadata
 mm = matching_metadata
@@ -336,13 +407,13 @@ data = [
 ("", "Filtered reads", ""),
 ("", d(m["reads_filtered"], m["reads"]), ""),
 ("", "Sequencing saturation", ""),
-("", s(round((1 - (m["umis_filtered"] / m["reads_filtered"]))*100, digits=1))*"%", ""),
+("", string(round((1 - (m["umis_filtered"] / m["reads_filtered"]))*100, digits=1))*"%", ""),
 ("", "Filtered UMIs", ""),
 ("", f(m["umis_filtered"]), ""),
 ("R1 UMI cutoff: ", "", "R2 UMI cutoff"),
 (f(m["R1_umicutoff"]), "", f(m["R2_umicutoff"])),
 ("R1 Barcodes", "R2:R1 ratio", "R2 Barcodes"),
-(f(m["R1_barcodes"]), s(round(m["R2_barcodes"]/m["R1_barcodes"],digits=2)), f(m["R2_barcodes"])),
+(f(m["R1_barcodes"]), string(round(m["R2_barcodes"]/m["R1_barcodes"],digits=2)), f(m["R2_barcodes"])),
 ("R1 exact matches", "", "R2 exact matches"),
 (d(mm["R1_exact"],m["umis_filtered"]), "", d(mm["R2_exact"],m["umis_filtered"])),
 ("R1 none matches", "", "R2 none matches"),
@@ -360,35 +431,26 @@ end
 hline!(p, [12.5], linestyle = :solid, color = :black)
 savefig(p, joinpath(out_path, "metadata.pdf"))
 
-merge_pdfs([joinpath(out_path,"elbows.pdf"), joinpath(out_path,"metadata.pdf")], joinpath(out_path,"QC.pdf"), cleanup=true)
+merge_pdfs([joinpath(out_path,"elbows.pdf"),
+            joinpath(out_path,"metadata.pdf"),
+            joinpath(out_path,"histograms.pdf")],
+            joinpath(out_path,"QC.pdf"), cleanup=true)
 
 @assert isempty(intersect(keys(metadata), keys(matching_metadata)))
 meta_df = DataFrame([Dict(:key => k, :value => v) for (k,v) in merge(metadata, matching_metadata)])
 sort!(meta_df, :key) ; meta_df = select(meta_df, :key, :value)
 CSV.write(joinpath(out_path,"metadata.csv"), meta_df, writeheader=false)
 
-# Write matrix and barcodes
-m1 = sort(collect(Set(df.sb1_i)))
-m2 = sort(collect(Set(df.sb2_i)))
-dict1 = Dict{UInt32, UInt32}(value => index for (index, value) in enumerate(m1))
-dict2 = Dict{UInt32, UInt32}(value => index for (index, value) in enumerate(m2))
-sb1_whitelist_short = sb1_whitelist[m1]
-sb2_whitelist_short = sb2_whitelist[m2]
-sb1_new = [dict1[k] for k in df.sb1_i]
-sb2_new = [dict2[k] for k in df.sb2_i]
-@assert sb1_whitelist[df.sb1_i] == sb1_whitelist_short[sb1_new]
-@assert sb2_whitelist[df.sb2_i] == sb2_whitelist_short[sb2_new]
-df.sb1_i = sb1_new
-df.sb2_i = sb2_new
-
-open(GzipCompressorStream, joinpath(out_path,"sb1.txt.gz"), "w") do file
-    for line in sb1_whitelist_short
-        write(file, line * "\n")
+@assert length(df1.umi) == length(sb1_whitelist_short)
+open(GzipCompressorStream, joinpath(out_path,"sb1.csv.gz"), "w") do file
+    for line in zip(sb1_whitelist_short, df1.umi)
+        write(file, line[1] * "," * string(line[2]) * "\n")
     end
 end
-open(GzipCompressorStream, joinpath(out_path,"sb2.txt.gz"), "w") do file
-    for line in sb2_whitelist_short
-        write(file, line * "\n")
+@assert length(df2.umi) == length(sb2_whitelist_short)
+open(GzipCompressorStream, joinpath(out_path,"sb2.csv.gz"), "w") do file
+    for line in zip(sb2_whitelist_short, df2.umi)
+        write(file, line[1] * "," * string(line[2]) * "\n")
     end
 end
 open(GzipCompressorStream, joinpath(out_path,"matrix.csv.gz"), "w") do file
@@ -397,4 +459,4 @@ end
 
 println("done!") ; flush(stdout) ; GC.gc()
 
-@assert all(f -> isfile(joinpath(out_path, f)), ["matrix.csv.gz", "sb1.txt.gz", "sb2.txt.gz", "metadata.csv", "QC.pdf"])
+@assert all(f -> isfile(joinpath(out_path, f)), ["matrix.csv.gz", "sb1.csv.gz", "sb2.csv.gz", "metadata.csv", "QC.pdf"])
