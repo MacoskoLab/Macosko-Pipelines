8807077f745cc00e1d4b4592051451fd0d56cad7
MatthewShabet <matthew.shabet@gmail.com>
2024-08-10 19:20:47 -0400

    Update recon.py

--- reconstruction/recon.py
+++ reconstruction/recon.py
@@ -8,28 +8,21 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from math import ceil
+from collections import Counter
 from scipy.sparse import coo_matrix
-from umap import UMAP
 from helpers import *
-
-# os.chdir("/home/nsachdev/recon/data/240609_SL-EXC_0308_A22KHFYLT3/D707-1234")
-# os.chdir("/home/nsachdev/recon/data/240411/H14_1")
-# os.chdir("/home/nsachdev/recon/data/240615_SL-EXG_0144_A22KH5WLT3/D703_D704_D705_D706")
-# os.chdir("/home/nsachdev/recon/data/240609_SL-EXC_0308_A22KHFYLT3/D702-1234_D703_D706_D707-5")
 
 def get_args():
     parser = argparse.ArgumentParser(description='process recon seq data')
     parser.add_argument("-i", "--in_dir", help="input data folder", type=str, default=".")
     parser.add_argument("-o", "--out_dir", help="output data folder", type=str, default=".")
     parser.add_argument("-gs", "--gspath", help="gcloud storage path to cache data output", type=str, default="")
-    # parser.add_argument("-c", "--core", help="define core type to use (CPU or GPU)", type=str, default="CPU")
+    parser.add_argument("-u", "--unit", help="define core type to use (CPU or GPU)", type=str, default="CPU")
     
     parser.add_argument("-l1", "--low1", help="R1 connection minimum", type=int, default=10)
     parser.add_argument("-l2", "--low2", help="R2 connection minimum", type=int, default=10)
-    parser.add_argument("-h1", "--high1", help="R1 connection maximum", type=int, default=3000)
-    parser.add_argument("-h2", "--high2", help="R2 connection maximum", type=int, default=3000)
-    
-    parser.add_argument("-a", "--algorithm", help="dimensionality reduction algo", type=str, default="UMAP")
+    parser.add_argument("-h1", "--high1", help="R1 connection maximum", type=int, default=1000)
+    parser.add_argument("-h2", "--high2", help="R2 connection maximum", type=int, default=1000)
     
     parser.add_argument("-n", "--n_neighbors", help="the number of neighboring points used for manifold approximation", type=int, default=45)
     parser.add_argument("-d", "--min_dist", help="the effective minimum distance between embedded points", type=float, default=0.1)
@@ -43,38 +36,37 @@ def get_args():
     return args
 
 args = get_args()
-algo = args.algorithm ; print(f"algorithm = {algo}")
-name = f"{algo}"
 
-if algo == "UMAP":
-    n_neighbors = args.n_neighbors ; print(f"n_neighbors = {n_neighbors}")
-    min_dist = args.min_dist       ; print(f"min_dist = {min_dist}")
-    init = args.init               ; print(f"init = {init}")
-    n_epochs = args.n_epochs       ; print(f"n_epochs = {n_epochs}")
-    name += f"_n={n_neighbors}_d={min_dist}_I={init}"
-    
-    connectivity = args.connectivity ; print(f"connectivity = {connectivity}")
-    assert connectivity in ["none", "nearest", "min_tree", "full_tree"]
-    if connectivity != "none":
-        n_neighbors2 = args.n_neighbors2 ; print(f"n_neighbors2 = {n_neighbors2}")
-        name += f"_c={connectivity.replace('_', '')}{n_neighbors2}"
-        assert n_neighbors2 <= n_neighbors
+in_dir = args.in_dir             ; print(f"input directory = {in_dir}")
+out_dir = args.out_dir           ; print(f"output directory = {out_dir}")
+gspath = args.gspath             ; print(f"gs:// output path = {gspath}")
+unit = args.unit                 ; print(f"processing unit = {unit}")
 
-l1 = args.low1  ; print(f"R1 connection minimum = {l1}")
-l2 = args.low2  ; print(f"R2 connection minimum = {l2}")
-h1 = args.high1 ; print(f"R1 connection maximum = {h1}")
-h2 = args.high2 ; print(f"R2 connection maximum = {h2}")
+l1 = args.low1                   ; print(f"R1 connection minimum = {l1}")
+l2 = args.low2                   ; print(f"R2 connection minimum = {l2}")
+h1 = args.high1                  ; print(f"R1 connection maximum = {h1}")
+h2 = args.high2                  ; print(f"R2 connection maximum = {h2}")
+
+n_neighbors = args.n_neighbors   ; print(f"n_neighbors = {n_neighbors}")
+min_dist = args.min_dist         ; print(f"min_dist = {min_dist}")
+init = args.init                 ; print(f"init = {init}")
+n_epochs = args.n_epochs         ; print(f"n_epochs = {n_epochs}")
+connectivity = args.connectivity ; print(f"connectivity = {connectivity}")
+n_neighbors2 = args.n_neighbors2 ; print(f"n_neighbors2 = {n_neighbors2}")
+
+name = f"UMAP_n={n_neighbors}_d={min_dist}_I={init}"
+
+if connectivity != "none":
+    assert connectivity in ["min_tree", "full_tree"]
+    name += f"_c={connectivity.replace('_', '')}{n_neighbors2}"
+    assert n_neighbors2 <= n_neighbors
+
 name += f"_c1={l1}-{h1}_c2={l2}-{h2}"
-
 print(f"name = {name}")
+out_dir = os.path.join(out_dir, name)
 
-in_dir = args.in_dir
 assert all(os.path.isfile(os.path.join(in_dir, file)) for file in ['matrix.csv.gz', 'sb1.csv.gz', 'sb2.csv.gz'])
-print(f"input directory = {in_dir}")
-
-out_dir = os.path.join(args.out_dir, name)
-if not os.path.exists(out_dir):
-    os.makedirs(out_dir)
+os.makedirs(out_dir, exist_ok=True)
 assert os.path.exists(out_dir)
 print(f"output directory = {out_dir}")
 
@@ -121,7 +113,7 @@ print(f"Final matrix dimension: {mat.shape}")
 
 # Get the previous embeddings
 print("\nDownloading previous embeddings...")
-file_path = os.path.join(args.gspath, name, "embeddings.npz")
+file_path = os.path.join(gspath, name, "embeddings.npz")
 print(f"Searching {file_path}...")
 try:
     import gcsfs
@@ -136,15 +128,37 @@ except Exception as e:
 
 sys.stdout.flush()
 
+### Compute the KNN ############################################################
+
+print("\nComputing the KNN...")
+knn_indices, knn_dists = knn_descent(np.log1p(mat), n_neighbors)
+np.savez_compressed(os.path.join(out_dir, "knn.npz"), indices=knn_indices, dists=knn_dists)
+knn = (knn_indices, knn_dists)
+
+if connectivity != "none":
+    mnn_indices, mnn_dists = mutual_nn_nearest(knn_indices, knn_dists, n_neighbors, n_neighbors2, connectivity)
+    np.savez_compressed(os.path.join(out_dir, "mnn.npz"), indices=mnn_indices, dists=mnn_dists)
+    assert np.all(np.isfinite(mnn_indices))
+    assert np.all(np.isfinite(mnn_dists))
+    n_neighbors = n_neighbors2
+    knn = (mnn_indices, mnn_dists)
+
 ### UMAP TIME ##################################################################
 
-def my_umap(mat, n_epochs, init=init):
+if unit.upper() == "CPU":
+    from umap import UMAP
+elif unit.upper() == "GPU":
+    from cuml.manifold.umap import UMAP
+else:
+    exit(f"Unrecognized --processing_unit flag {unit}")
+
+def my_umap(mat, knn, n_epochs, init=init):
     reducer = UMAP(n_components = 2,
                    metric = "cosine",
                    spread = 1.0,
                    random_state = None,
                    verbose = True,
-                   precomputed_knn = (knn_indices, knn_dists),
+                   precomputed_knn = knn,
                    
                    n_neighbors = n_neighbors,
                    min_dist = min_dist,
@@ -154,64 +168,38 @@ def my_umap(mat, n_epochs, init=init):
     embedding = reducer.fit_transform(np.log1p(mat))
     return(embedding)
 
-if algo == "UMAP":
-    
-    print("\nComputing the KNN...")
-    knn_indices, knn_dists = knn_descent(np.log1p(mat), n_neighbors)
-    
-    if connectivity != "none":
-        knn_indices, knn_dists = mutual_nn_nearest(knn_indices, knn_dists, n_neighbors, n_neighbors2, connectivity)
-        assert np.all(np.isfinite(knn_indices))
-        assert np.all(np.isfinite(knn_dists))
-        n_neighbors = n_neighbors2
-    
-    print("\nRunning UMAP...")
-    if len(embeddings) == 0:
-        embeddings.append(my_umap(mat, n_epochs=20))
-        embeddings.append(my_umap(mat, n_epochs=80, init=embeddings[-1]))
-        embeddings.append(my_umap(mat, n_epochs=900, init=embeddings[-1]))
-    else:
-        embeddings.append(my_umap(mat, n_epochs=1000))
-    
-    for i in range(ceil(n_epochs/1000)-1):
-        # # Upload intermediate embeddings
-        # try:
-        #     import gcsfs
-        #     file_path = os.path.join(args.gspath, name, "embeddings.npz")
-        #     with gcsfs.GCSFileSystem().open(file_path, 'wb') as f:
-        #         np.savez(f, **{f"arr_{i}":e for i,e in enumerate(embeddings)})
-        #     print("Intermediate embeddings successfully uploaded")
-        # except Exception as e:
-        #     print(f"Unable to upload intermediate embeddings: {str(e)}")
-
-        # Run more umap
-        print(i+2)
-        embeddings.append(my_umap(mat, init=embeddings[-1], n_epochs=1000))
+print("\nRunning UMAP...")
+embeddings.append(my_umap(mat, knn, n_epochs=1000))    
+for i in range(ceil(n_epochs/1000)-1):
+    print(i+2)
+    # # Upload intermediate embeddings
+    # try:
+    #     import gcsfs
+    #     file_path = os.path.join(gspath, name, "embeddings.npz")
+    #     with gcsfs.GCSFileSystem().open(file_path, 'wb') as f:
+    #         np.savez(f, **{f"arr_{i}":e for i,e in enumerate(embeddings)})
+    #     print("Intermediate embeddings successfully uploaded")
+    # except Exception as e:
+    #     print(f"Unable to upload intermediate embeddings: {str(e)}")
+    embeddings.append(my_umap(mat, knn, init=embeddings[-1], n_epochs=1000))
 
 print("\nWriting results...")
+embedding = embeddings[-1]
 
 # Save the embeddings
-np.savez(os.path.join(out_dir, "embeddings.npz"), *embeddings)
-
-# Plot the final UMAP
-fig, ax = plt.subplots(figsize=(10, 8))
-x, y = embeddings[-1][:, 0], embeddings[-1][:, 1]
-hb = ax.hexbin(x, y, cmap='viridis', linewidths=0.1)
-cb = fig.colorbar(hb, ax=ax, shrink = 0.75)
-ax.set_title(f'umap hexbin ({embeddings[-1].shape[0]:,} anchor beads) [{(len(embeddings)-2)*1000} epochs]')
-ax.set_xlim(x.min(), x.max())
-ax.set_ylim(y.min(), y.max())
-ax.axis('equal')
-plt.tight_layout()
-fig.savefig(os.path.join(out_dir, "umap.png"), dpi=200)
-plt.close(fig)
+np.savez_compressed(os.path.join(out_dir, "embeddings.npz"), *embeddings)
 
 # Create the Puck file
 sbs = [sb2["sb2"][i] for i in uniques2]
-assert embeddings[-1].shape[0] == len(sbs)
+assert embedding.shape[0] == len(sbs)
 with open(os.path.join(out_dir, "Puck.csv"), mode='w', newline='') as file:
     writer = csv.writer(file)
     for i in range(len(sbs)):
-        writer.writerow([sbs[i], embeddings[-1][i,0], embeddings[-1][i,1]])
+        writer.writerow([sbs[i], embedding[i,0], embedding[i,1]])
 
 print("\nDone!")
+
+title = f"umap hexbin ({embedding.shape[0]:} anchor beads) [{(len(embeddings))*1000} epochs]"
+fig, ax = hexmap(embedding, title)
+fig.savefig(os.path.join(out_dir, "umap.png"), dpi=200)
+plt.close(fig)
