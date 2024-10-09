fc921c5eeb77d56be8648d461aebdd781006b825
MatthewShabet <matthew.shabet@gmail.com>
2024-10-09 11:05:29 -0400

    Update jupyterlab.sh

--- uger/jupyterlab.sh
+++ uger/jupyterlab.sh
@@ -6,8 +6,14 @@ echo "***************"
 echo "$HOST_IP:$PORT_NUM"
 echo "***************"
 
-trap "echo TRAPTRIGGERED ; podman stop jupyterlab$PORT_NUM ; exit" SIGTERM SIGINT
+ trap 'echo signal recieved in BATCH!; kill -15 "${PID}"; wait "${PID}";' SIGINT SIGTERM
+trap 'echo signal recieved in BATCH!' SIGINT SIGTERM
+podman inspect --format '{{.State.Pid}}' $CONTAINER_ID
+
 
 IMAGE='us-central1-docker.pkg.dev/velina-208320/docker-count/img:latest'
-JUPYTER_CMD="source ~/.profile ; micromamba activate base ; jupyter lab -y --allow-root --ip='*' --port='8888' --NotebookApp.token='' --NotebookApp.password='' --Session.key='' --no-browser"
-podman run --rm --name jupyterlab$PORT_NUM -v /broad/macosko/data/discopipeline:/discopipeline -p $PORT_NUM:8888 --entrypoint "/bin/bash" $IMAGE -c "$JUPYTER_CMD"
+JUPYTER_CMD="source ~/.profile ; micromamba activate base ; jupyter lab --allow-root --ip='*' --port='8888' --NotebookApp.token='' --NotebookApp.password='' --Session.key='' --no-browser"
+CONTAINER_ID=$(podman run --rm --detach -v /broad/macosko/data/discopipeline:/discopipeline -p $PORT_NUM:8888 --entrypoint "/bin/bash" $IMAGE -c "$JUPYTER_CMD")
+CONTAINER_PID=$(podman inspect --format '{{.State.Pid}}' $CONTAINER_ID)
+
+wait $CONTAINER_PID
