6e5459d745609e2aa31254718804f61397790b85
MatthewShabet <matthew.shabet@gmail.com>
2024-10-08 23:12:56 -0400

    Create jupyterlab.sh

---
+++ uger/jupyterlab.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+HOST_IP=$(hostname -i)
+PORT_NUM=$(for port in {8700..9000}; do ss -tuln | grep -q ":$port " || { echo "$port"; break; }; done)
+echo "***************"
+echo "$HOST_IP:$PORT_NUM"
+echo "***************"
+
+IMAGE='us-central1-docker.pkg.dev/velina-208320/docker-count/img:latest'
+JUPYTER_CMD="source ~/.profile ; micromamba activate base ; jupyter lab -y --allow-root --ip='*' --port='8888' --NotebookApp.token='' --NotebookApp.password='' --Session.key='' --no-browser"
+podman run --rm -v /broad/macosko/data/discopipeline:/discopipeline -p $PORT_NUM:8888 --entrypoint "/bin/bash" $IMAGE -c "$JUPYTER_CMD"
