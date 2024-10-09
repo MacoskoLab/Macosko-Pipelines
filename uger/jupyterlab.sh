f5a172f01a0ea7e1edaab3504825d11488128ae9
MatthewShabet <matthew.shabet@gmail.com>
2024-10-09 00:12:22 -0400

    Update jupyterlab.sh

--- uger/jupyterlab.sh
+++ uger/jupyterlab.sh
@@ -6,6 +6,8 @@ echo "***************"
 echo "$HOST_IP:$PORT_NUM"
 echo "***************"
 
+trap "echo TRAPTRIGGERED ; podman stop jupyterlab$PORT_NUM ; exit" SIGTERM SIGINT
+
 IMAGE='us-central1-docker.pkg.dev/velina-208320/docker-count/img:latest'
 JUPYTER_CMD="source ~/.profile ; micromamba activate base ; jupyter lab -y --allow-root --ip='*' --port='8888' --NotebookApp.token='' --NotebookApp.password='' --Session.key='' --no-browser"
-podman run --rm -v /broad/macosko/data/discopipeline:/discopipeline -p $PORT_NUM:8888 --entrypoint "/bin/bash" $IMAGE -c "$JUPYTER_CMD"
+podman run --rm --name jupyterlab$PORT_NUM -v /broad/macosko/data/discopipeline:/discopipeline -p $PORT_NUM:8888 --entrypoint "/bin/bash" $IMAGE -c "$JUPYTER_CMD"
