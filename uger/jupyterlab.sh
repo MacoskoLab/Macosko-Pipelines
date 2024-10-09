27446c2738b283d20ea41ffbad5db1c84fcaf5a1
MatthewShabet <matthew.shabet@gmail.com>
2024-10-09 18:44:52 -0400

    Update jupyterlab.sh

--- uger/jupyterlab.sh
+++ uger/jupyterlab.sh
@@ -6,14 +6,11 @@ echo "***************"
 echo "$HOST_IP:$PORT_NUM"
 echo "***************"
 
- trap 'echo signal recieved in BATCH!; kill -15 "${PID}"; wait "${PID}";' SIGINT SIGTERM
-trap 'echo signal recieved in BATCH!' SIGINT SIGTERM
-podman inspect --format '{{.State.Pid}}' $CONTAINER_ID
-
-
 IMAGE='us-central1-docker.pkg.dev/velina-208320/docker-count/img:latest'
 JUPYTER_CMD="source ~/.profile ; micromamba activate base ; jupyter lab --allow-root --ip='*' --port='8888' --NotebookApp.token='' --NotebookApp.password='' --Session.key='' --no-browser"
-CONTAINER_ID=$(podman run --rm --detach -v /broad/macosko/data/discopipeline:/discopipeline -p $PORT_NUM:8888 --entrypoint "/bin/bash" $IMAGE -c "$JUPYTER_CMD")
-CONTAINER_PID=$(podman inspect --format '{{.State.Pid}}' $CONTAINER_ID)
+exec podman run --rm -v /broad/macosko/data/discopipeline:/discopipeline -p $PORT_NUM:8888 --entrypoint "/bin/bash" $IMAGE -c "$JUPYTER_CMD"
 
-wait $CONTAINER_PID
+# Add this to your .my.bashrc
+jupyterlab() {
+    srun --disable-status "$@" /broad/macosko/data/discopipeline/scripts/jupyterlab.sh
+}
