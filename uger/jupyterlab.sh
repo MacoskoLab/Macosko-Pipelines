7d632593649ccdd555f5d23289cf644153fdcebf
MatthewShabet <matthew.shabet@gmail.com>
2024-10-09 22:41:45 -0400

    Update jupyterlab.sh

--- uger/jupyterlab.sh
+++ uger/jupyterlab.sh
@@ -8,7 +8,7 @@ echo "***************"
 
 IMAGE='us-central1-docker.pkg.dev/velina-208320/docker-count/img:latest'
 JUPYTER_CMD="source ~/.profile ; micromamba activate base ; jupyter lab --allow-root --ip='*' --port='8888' --NotebookApp.token='' --NotebookApp.password='' --Session.key='' --no-browser"
-exec podman run --rm -v /broad/macosko/data/discopipeline:/discopipeline -p $PORT_NUM:8888 --entrypoint "/bin/bash" $IMAGE -c "$JUPYTER_CMD"
+exec podman run --rm -v /broad/macosko/data/discopipeline:/discopipeline -p $PORT_NUM:8888 --entrypoint "/bin/bash" "$IMAGE" -c "$JUPYTER_CMD"
 
 # Add this to your .my.bashrc
 jupyterlab() {
