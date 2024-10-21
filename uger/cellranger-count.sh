ad99c59c9f22fe32900efef060b8b9a78ad7d115
MatthewShabet <matthew.shabet@gmail.com>
2024-10-21 00:12:25 -0400

    Update cellranger-count.sh

--- uger/cellranger-count.sh
+++ uger/cellranger-count.sh
@@ -9,26 +9,28 @@ fi
 # Assert no spaces
 for arg in "$@"; do
     if [[ "$arg" =~ " " ]]; then
-        echo "Error: Argument '$arg' contains spaces."
+        echo "ERROR: Argument '$arg' contains spaces"
         exit 1
     fi
 done
 
-BCL="$1"
-INDEX="$2"
-TRANSCRIPTOME="$3"
-CHEMISTRY="$4"
+BCL="$1"           ; echo $BCL
+INDEX="$2"         ; echo $INDEX
+TRANSCRIPTOME="$3" ; echo $TRANSCRIPTOME
+CHEMISTRY="$4"     ; echo $CHEMISTRY
 
 ROOT="/broad/macosko/data/discopipeline"
 BINARY="/broad/macosko/data/discopipeline/software/cellranger-8.0.1/bin/cellranger"
 
-SBATCH_PARAMS="-C=RedHat7 -o=$ROOT/logs/$BCL/$INDEX/cellranger-count.out -J=cellranger-count-$INDEX -c=32 --mem=96G -t 96:00:00"
-CELLRANGER_PARAMS="--id=$INDEX \
-                   --output-dir=$ROOT/cellranger-count/$BCL \
-                   --transcriptome=$ROOT/references/$TRANSCRIPTOME \
-                   --fastqs=$ROOT/fastqs/$BCL \
-                   --sample=$INDEX \
-                   --chemistry=$CHEMISTRY \
-                   --create-bam=true --include-introns=true --nosecondary"
+SBATCH_PARAMS="-C RedHat7 -o $ROOT/logs/$BCL/$INDEX/cellranger-count.out -J cellranger-count-$INDEX -c 32 --mem 96G -t 96:00:00"
+CELLRANGER_PARAMS="--id $INDEX \
+                   --output-dir $ROOT/cellranger-count/$BCL \
+                   --transcriptome $ROOT/references/$TRANSCRIPTOME \
+                   --fastqs $ROOT/fastqs/$BCL \
+                   --sample $INDEX \
+                   --chemistry $CHEMISTRY \
+                   --create-bam true \
+                   --include-introns true \
+                   --nosecondary"
 
 sbatch $SBATCH_PARAMS $BINARY count $CELLRANGER_PARAMS
