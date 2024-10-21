05e1091be7e02386581d10740c8c27cd50e3a71a
MatthewShabet <matthew.shabet@gmail.com>
2024-10-21 00:27:44 -0400

    Update cellranger-count.sh

--- uger/cellranger-count.sh
+++ uger/cellranger-count.sh
@@ -6,23 +6,25 @@ if [ "$#" -ne 4 ]; then
     exit 1
 fi
 
-# Assert no spaces
-for arg in "$@"; do
-    if [[ "$arg" =~ " " ]]; then
-        echo "ERROR: Argument '$arg' contains spaces"
-        exit 1
-    fi
-done
-
 BCL="$1"           ; echo $BCL
 INDEX="$2"         ; echo $INDEX
 TRANSCRIPTOME="$3" ; echo $TRANSCRIPTOME
 CHEMISTRY="$4"     ; echo $CHEMISTRY
 
+# Assert no whitespace
+for arg in "$@"; do
+    if [[ "$arg" =~ [[:space:]] ]]; then
+        echo "ERROR: Argument '$arg' contains whitespace"
+        exit 1
+    fi
+done
+
 ROOT="/broad/macosko/data/discopipeline"
 BINARY="/broad/macosko/data/discopipeline/software/cellranger-8.0.1/bin/cellranger"
 
-SBATCH_PARAMS="-C RedHat7 -o $ROOT/logs/$BCL/$INDEX/cellranger-count.out -J cellranger-count-$INDEX -c 32 --mem 96G -t 96:00:00"
+SBATCH_PARAMS="-C RedHat7 -o $ROOT/logs/$BCL/$INDEX/cellranger-count.log -J cellranger-count-$BCL-$INDEX \
+               -c 32 --mem 96G --time 96:00:00 \
+               --mail-user mshabet@broadinstitute.org --mail-type END,FAIL,REQUEUE,INVALID_DEPEND,STAGE_OUT,TIME_LIMIT"
 CELLRANGER_PARAMS="--id $INDEX \
                    --output-dir $ROOT/cellranger-count/$BCL \
                    --transcriptome $ROOT/references/$TRANSCRIPTOME \
