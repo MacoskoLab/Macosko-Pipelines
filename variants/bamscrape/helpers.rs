2d0f65330f16aa237a4dedf1d95cc4099abc740a
MatthewShabet <matthew.shabet@gmail.com>
2024-11-12 03:31:12 -0500

    Update helpers.rs

--- variants/bamscrape/helpers.rs
+++ variants/bamscrape/helpers.rs
@@ -49,16 +49,16 @@ impl<T: PrimInt> WriteVector for [T] {
     fn write_vector(&self, group: &hdf5::Group, name: &str) {
         let max: u128 = self.iter().max().expect("ERROR: blank").to_u128().expect("ERROR: not u128");
         match max {
-            0..=255            => group.new_dataset_builder().with_data(&self.into_iter().map(|x| x.to_u8().expect("ERROR: not u8")).collect::<Vec<u8>>()).create(name).expect(".h5 error"),
-            256..=65535        => group.new_dataset_builder().with_data(&self.into_iter().map(|x| x.to_u16().expect("ERROR: not u16")).collect::<Vec<u16>>()).create(name).expect(".h5 error"),
-            65536..=4294967295 => group.new_dataset_builder().with_data(&self.into_iter().map(|x| x.to_u32().expect("ERROR: not u32")).collect::<Vec<u32>>()).create(name).expect(".h5 error"),
-            _                  => group.new_dataset_builder().with_data(&self.into_iter().map(|x| x.to_u64().expect("ERROR: not u64")).collect::<Vec<u64>>()).create(name).expect(".h5 error"),
+            0..=255            => group.new_dataset_builder().deflate(1).with_data(&self.into_iter().map(|x| x.to_u8().expect("ERROR: not u8")).collect::<Vec<u8>>()).create(name).expect(".h5 error"),
+            256..=65535        => group.new_dataset_builder().deflate(1).with_data(&self.into_iter().map(|x| x.to_u16().expect("ERROR: not u16")).collect::<Vec<u16>>()).create(name).expect(".h5 error"),
+            65536..=4294967295 => group.new_dataset_builder().deflate(1).with_data(&self.into_iter().map(|x| x.to_u32().expect("ERROR: not u32")).collect::<Vec<u32>>()).create(name).expect(".h5 error"),
+            _                  => group.new_dataset_builder().deflate(1).with_data(&self.into_iter().map(|x| x.to_u64().expect("ERROR: not u64")).collect::<Vec<u64>>()).create(name).expect(".h5 error"),
         };
     }
 }
 impl<T: AsRef<[u8]>> WriteVector for Vec<T> {
     fn write_vector(&self, group: &hdf5::Group, name: &str) {
         let vec: Vec<VarLenAscii> = self.iter().map(|s| VarLenAscii::from_ascii(s).expect("ERROR: non-ascii character")).collect();
-        group.new_dataset_builder().with_data(&vec).create(name).expect(".h5 error");
+        group.new_dataset_builder().deflate(1).with_data(&vec).create(name).expect(".h5 error");
     }
 }
