763bb7b9122f9b861a9dd663c9ae95d8ba57e5e3
MatthewShabet <matthew.shabet@gmail.com>
2025-04-25 03:54:09 -0400

    Update recon.py

--- reconstruction/recon.py
+++ reconstruction/recon.py
@@ -15,9 +15,7 @@ def get_args():
     parser.add_argument("-o", "--out_dir", help="output data folder", type=str, default=".")
     parser.add_argument("-c", "--cores", type=int, default=-1)
     parser.add_argument("-b", "--bead", type=int, default=2)
-
-    parser.add_argument("-K", "--knn_filter", help="k-nearest neighbor filter", type=bool, default=False)
-    parser.add_argument("-M", "--mnn", help="mutual nearest neighbors", type=bool, default=False)
+    parser.add_argument("-K", "--knn_filter", help="KNN graph filter", type=bool, default=False)
     
     parser.add_argument("-nn", "--n_neighbors", type=int, default=45)
     parser.add_argument("-md", "--min_dist", type=float, default=0.1)
@@ -37,9 +35,7 @@ in_dir = args.in_dir             ; print(f"input directory: {in_dir}")
 out_dir = args.out_dir           ; print(f"output directory: {out_dir}")
 cores = args.cores               ; print(f"cores: {cores}")
 bead = args.bead                 ; print(f"bead: {bead}")
-
 knn_filter = args.knn_filter     ; print(f"KNN filter: {knn_filter}")
-mnn = args.mnn                   ; print(f"MNN: {mnn}")
 
 opts = dict()
 opts["n_neighbors"] = args.n_neighbors
@@ -49,18 +45,17 @@ opts["local_connectivity"] = args.local_connectivity
 opts["repulsion_strength"] = args.repulsion_strength
 opts["negative_sample_rate"] = args.negative_sample_rate
 opts["n_epochs"] = args.n_epochs
-print(f"UMAP options:\n{'\n'.join(f'    {k} = {v}' for k, v in opts.items())}")
+print("UMAP options:\n" + '\n'.join(f'    {k} = {v}' for k, v in opts.items()))
 # learning_rate, set_op_mix_ratio, disconnection_distance
 
 # Create programmatic output folder name
-name = f"UMAP_sb{bead}_"
+name = f"UMAP{bead}_"
 name += "_".join([f"{''.join(s[0] for s in k.split('_'))}{v}" for k,v in opts.items()])
-name += "_" if mnn or knn_filter else ""
-name += ("M" if mnn else "") + ("K" if knn_filter else "")
+name += ("_K" if knn_filter else "")
 print(f"name: {name}")
 
 # Check in/out directories
-assert all(os.path.isfile(os.path.join(in_dir, file)) for file in ['matrix.csv.gz', 'sb1.txt.gz', 'sb2.txt.gz', f'knn{bead}.npz'])
+assert all(os.path.isfile(os.path.join(in_dir, file)) for file in ['sb1.txt.gz', 'sb2.txt.gz', f'knn{bead}.npz'])
 out_dir = os.path.join(out_dir, name)
 if os.path.exists(out_dir):
     print(f"WARNING: output {out_dir} exists, overwriting...")
@@ -85,99 +80,81 @@ print("\nLoading KNN...")
 # Read KNN matrix
 knn_matrix = sp.load_npz(os.path.join(in_dir, f'knn{bead}.npz')).tocsr()
 knn_matrix.setdiag(0) ; knn_matrix.eliminate_zeros()
-k = np.diff(knn_matrix.indptr).max()+1 # +1 because K includes self-node
+k = np.diff(knn_matrix.indptr).max() + 1 # +1 because K includes self-node
 assert opts["n_neighbors"] <= k
 
 knn_mask = KNNMask(knn_matrix)
 
 # Prune non-reciprocated edges
-if mnn:
-    print(f"Running MNN with {k} neighbors...")
-    m = np.abs(knn_matrix - knn_matrix.T) > np.min(knn_matrix.data)/2
-    knn_matrix[m] = 0
-    knn_matrix.eliminate_zeros()
-    del m ; gc.collect()
+# m = np.abs(knn_matrix - knn_matrix.T) > np.min(knn_matrix.data)/2
+# knn_matrix[m] = 0
+# knn_matrix.eliminate_zeros()
+# del m ; gc.collect()
 
-print("Filtering beads with no neighbors...")
+print("\nFiltering beads with no neighbors...")
 m = np.diff(knn_matrix.indptr) != 0
-print(f"{sum(~m)} beads removed")
+print(f"{np.sum(~m)} beads removed")
 knn_matrix = knn_mask.apply_mask(knn_matrix, m)
 del m ; gc.collect()
 
-print(f"Reducing KNN from {k} to {opts['n_neighbors']} neighbors...")
-knn_matrix = csr_k_nearest(knn_matrix, opts['n_neighbors']-1)
+print(f"\nFiltering beads with <{opts['n_neighbors']} neighbors...")
+while any(np.diff(knn_matrix.indptr) < opts['n_neighbors'] - 1):
+    m = np.diff(knn_matrix.indptr) >= opts['n_neighbors'] - 1
+    print(f"{np.sum(~m)} beads removed")
+    knn_matrix = knn_mask.apply_mask(knn_matrix, m)
+    del m ; gc.collect()
+
+print(f"\nReducing KNN from {k} to {opts['n_neighbors']} neighbors...")
+knn_matrix = csr_k_nearest(knn_matrix, opts['n_neighbors'] - 1)
 gc.collect()
 
-print("Creating KNN graph...")
+print("\nRemoving disconnected components...")
+n_components, labels = sp.csgraph.connected_components(knn_matrix, directed=False, return_labels=True)
+m = labels == np.bincount(labels).argmax()
+knn_matrix = knn_mask.apply_mask(knn_matrix, m)
+print(f"{np.sum(~m)} beads removed")
+del m, n_components, labels ; gc.collect()
+assert all(np.diff(knn_matrix.indptr) >= opts['n_neighbors'] - 1)
+
+print("\nCreating KNN graph...")
+knn_matrix = knn_matrix.maximum(knn_matrix.T)
 G = ig.Graph(n = knn_matrix.shape[0],
              edges = zip(*knn_matrix.nonzero()),
              edge_attrs = {'weight': knn_matrix.data},
              directed = False)
-G.simplify(combine_edges="max")
+G.simplify(multiple=True, loops=True, combine_edges="max")
+
+print("\nPlotting KNN metrics...")
+knn_indices, knn_dists = knn_matrix2indist(knn_matrix)
+nnd = knn_dists[:,1] ; nnd = nnd[np.isfinite(nnd)]
+fnd = knn_dists[:,opts['n_neighbors']-1] ; fnd = fnd[np.isfinite(fnd)]
+ies = np.array(G.degree(mode="in"))
+tlu = np.array(G.transitivity_local_undirected(mode="zero", weights="weight"))
+# con = np.array(G.constraint(weights="weight"))
+# nss = np.array(G.neighborhood_size(order=2))
+# sts = np.array(G.strength(weights="weight"))
+fig = knn_plot(nnd, fnd, ies, tlu, opts['n_neighbors'])
+fig.savefig(os.path.join(out_dir, "knn.pdf"), dpi=200)
+del fig, knn_indices, knn_dists
 
 if knn_filter:
     print("Running KNN filter...")
-    assert False, "TODO: incorporate new collision detector"
-    #cc = G.connected_components()
-    #tlu = G.transitivity_local_undirected(mode="zero", weights="weight")
 
-    # # Remove weakly-connected components
-    # print("Removing WCCs...")
-    # n_components, labels = sp.csgraph.connected_components(knn_matrix, directed=True, connection='strong', return_labels=True)
-    # m = labels == np.bincount(labels).argmax()
-    # knn_matrix = knn_mask.apply_mask(knn_matrix, m)
-    # print(f"{np.sum(~m)} WCC beads removed")
-    # del m ; gc.collect()
-
-    # # Filter low clustering coefficient
-    # clustering = nx.clustering(G, nodes=None, weight=None)
-    # data = [clustering[key] for key in sorted(clustering.keys())]
-    # hist_z(axes[1,1], data, z_low=z_low)
-    # axes[1,1].set_xlabel('Clustering coefficient')
-    # axes[1,1].set_title('Local clustering coefficient')
-
-    # Filter beads with far nearest neighbors (?)
-    # Filter too-high or too-low in-edges (?)
-    # this method needs to update knn_mask, knn_matrix, and G
-    # makes sure the node order in G continues to match knn_matrix
-    # print("X total beads removed")
-
-knn_indices, knn_dists = knn_matrix2indist(knn_matrix)
-del knn_matrix ; gc.collect()
-
-# KNN graph plot (TODO)
-tlu = G.transitivity_local_undirected(mode="zero", weights="weight")
-
-# KNN indist plot
-fig, ax = knn_plot(knn_indices, knn_dists, tlu)
-fig.savefig(os.path.join(out_dir, "knn.pdf"), dpi=200)
-del fig, ax
-
-# Unused metrics
-# G.strength(G.vs, weights="weight")
-# G.reciprocity()
-# G.personalized_pagerank()
-# G.path_length_hist()
-# G.neighborhood_size()
-# G.neighborhood()
-# G.knn()
-# G.hub_score()
-# G.diversity()
-# G.degree()
-# G.coreness()
-# G.constraint()
-# G.cocitation()
-# G.closeness()
-# G.cliques()
-# G.bibcoupling()
-# G.betweenness()
-# G.authority_score()
-# G.assortativity()
+    # Filter low clustering coefficient (<0.2)
+    m = tlu>=0.2
+    knn_matrix = knn_mask.apply_mask(knn_matrix, m)
+    G = G.subgraph(np.where(m)[0])
+    print(f"{np.sum(~m)} beads removed")
+    del m ; gc.collect()
+    
+    # Filter beads with far nearest neighbors?
+    # Filter too-high or too-low in-edges?
+    # New collision detector?
 
 # at this point, we have:
-#   final G (for leiden)
-#   final knn_indices/knn_dists (for umap)
-#   final knn_mask (for matrix, sb2)
+#  final G (for leiden)
+#  final knn_matrix (for umap)
+#  final knn_mask (for sb)
 
 ### Initialization #############################################################
 
@@ -267,39 +244,9 @@ fig.savefig(os.path.join(out_dir, "leiden.pdf"), dpi=200)
 del fig, ax
 
 np.savez_compressed(os.path.join(out_dir, 'leiden.npz'), embedding=embedding, membership=membership)
-# save G too? del G?
+del G
 
 ### Load + subset data #########################################################
-
-# Load matrix
-print('\nLoading the matrix...')
-df = pd.read_csv(os.path.join(in_dir, 'matrix.csv.gz'), compression='gzip')
-df.sb1_index -= 1 # convert from 1- to 0-indexed
-df.sb2_index -= 1 # convert from 1- to 0-indexed
-# assert set(df.sb1_index) == set(range(sb1.shape[0]))
-# assert set(df.sb2_index) == set(range(sb2.shape[0]))
-
-if bead == 1:
-    mat = sp.csr_matrix((df['umi'], (df['sb1_index'], df['sb2_index'])))
-if bead == 2:
-    mat = sp.csr_matrix((df['umi'], (df['sb2_index'], df['sb1_index'])))
-del df ; gc.collect()
-
-# Subset matrix
-assert len(knn_mask.mask) == mat.shape[0]
-assert type(mat) == sp._csr.csr_matrix
-mat = mat[knn_mask.mask]
-print(f"Final matrix dimension: {mat.shape}")
-print(f"Final matrix size: {csr_nbytes(mat)/1024/1024:.2f} MiB")
-gc.collect()
-
-# Fill in -1 KNN elements with neighbors of neighbors
-# from sklearn.metrics.pairwise import cosine_similarity
-# b_list = np.where(np.any(knn_indices == -1, axis=1))[0]
-# print(f"Filling out neighbors for {len(b_list)} beads")
-# for b in b_list:
-#     ns = G.neighborhood(b, order=2, mindist=2)
-#     cosine_similarity(mat[b], mat[ns])
 
 # Load barcodes
 print('Loading the barcodes...')
@@ -311,20 +258,21 @@ assert type(sb) == pd.core.frame.DataFrame
 sb = sb[knn_mask.mask]
 gc.collect()
 
-assert mat.shape[0] == knn_indices.shape[0] == knn_dists.shape[0] == sb.shape[0]
+#assert mat.shape[0] == knn_indices.shape[0] == knn_dists.shape[0] == sb.shape[0]
+assert knn_matrix.shape[0] == knn_matrix.shape[1] == sb.shape[0]
 
 ### UMAP TIME ##################################################################
 
 print("\nRunning UMAP...")
 embeddings = []
 if opts["n_epochs"] <= 1000:
-    embeddings.append(my_umap(mat, (knn_indices, knn_dists), init, opts, cores))
+    embeddings.append(my_umap(knn_matrix, init, opts, cores))
 else:
     n_epochs = opts["n_epochs"]
     opts["n_epochs"] = 1000
-    embeddings.append(my_umap(mat, (knn_indices, knn_dists), init, opts, cores))
+    embeddings.append(my_umap(knn_matrix, init, opts, cores))
     for i in range(int(np.ceil(n_epochs/1000))-1):
-        embeddings.append(my_umap(mat, (knn_indices, knn_dists), embeddings[-1], opts, cores))
+        embeddings.append(my_umap(knn_matrix, embeddings[-1], opts, cores))
     opts["n_epochs"] = n_epochs
 
 ### WRITE RESULTS ##############################################################
