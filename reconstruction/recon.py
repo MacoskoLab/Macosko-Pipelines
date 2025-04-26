1ed97a85bec152b9ee072ec57f9c9738d64936a1
MatthewShabet <matthew.shabet@gmail.com>
2025-04-26 02:59:17 -0400

    Update recon.py

--- reconstruction/recon.py
+++ reconstruction/recon.py
@@ -17,7 +17,7 @@ def get_args():
     parser.add_argument("-b", "--bead", type=int, default=2)
     parser.add_argument("-K", "--knn_filter", help="KNN graph filter", type=bool, default=False)
     
-    parser.add_argument("-nn", "--n_neighbors", type=int, default=45)
+    parser.add_argument("-nn", "--n_neighbors", type=int, default=30)
     parser.add_argument("-md", "--min_dist", type=float, default=0.1)
     parser.add_argument("-s", "--spread", type=float, default=1.0)
     parser.add_argument("-lc", "--local_connectivity", type=int, default=1)
@@ -80,8 +80,7 @@ print("\nLoading KNN...")
 # Read KNN matrix
 knn_matrix = sp.load_npz(os.path.join(in_dir, f'knn{bead}.npz')).tocsr()
 knn_matrix.setdiag(0) ; knn_matrix.eliminate_zeros()
-k = np.diff(knn_matrix.indptr).max() + 1 # +1 because K includes self-node
-assert opts["n_neighbors"] <= k
+assert np.diff(knn_matrix.indptr).max() >= opts["n_neighbors"] - 1
 
 knn_mask = KNNMask(knn_matrix)
 
@@ -94,42 +93,33 @@ knn_mask = KNNMask(knn_matrix)
 print("\nFiltering beads with no neighbors...")
 m = np.diff(knn_matrix.indptr) != 0
 print(f"{np.sum(~m)} beads removed")
-knn_matrix = knn_mask.apply_mask(knn_matrix, m)
+knn_matrix = knn_mask.apply_mask(knn_matrix, m, "No neighbors")
 del m ; gc.collect()
 
-print(f"\nFiltering beads with <{opts['n_neighbors']} neighbors...")
-while any(np.diff(knn_matrix.indptr) < opts['n_neighbors'] - 1):
-    m = np.diff(knn_matrix.indptr) >= opts['n_neighbors'] - 1
-    print(f"{np.sum(~m)} beads removed")
-    knn_matrix = knn_mask.apply_mask(knn_matrix, m)
-    del m ; gc.collect()
-
-print(f"\nReducing KNN from {k} to {opts['n_neighbors']} neighbors...")
+print(f"\nReducing KNN to {opts['n_neighbors']} neighbors...")
 knn_matrix = csr_k_nearest(knn_matrix, opts['n_neighbors'] - 1)
 gc.collect()
 
 print("\nRemoving disconnected components...")
 n_components, labels = sp.csgraph.connected_components(knn_matrix, directed=False, return_labels=True)
 m = labels == np.bincount(labels).argmax()
-knn_matrix = knn_mask.apply_mask(knn_matrix, m)
+knn_matrix = knn_mask.apply_mask(knn_matrix, m, "Disconnected")
 print(f"{np.sum(~m)} beads removed")
 del m, n_components, labels ; gc.collect()
-assert all(np.diff(knn_matrix.indptr) >= opts['n_neighbors'] - 1)
 
 print("\nCreating KNN graph...")
-knn_matrix = knn_matrix.maximum(knn_matrix.T)
 G = ig.Graph(n = knn_matrix.shape[0],
              edges = zip(*knn_matrix.nonzero()),
-             edge_attrs = {'weight': knn_matrix.data},
+             #edge_attrs = {'dist': knn_matrix.data, 'sim': 1-knn_matrix.data},
              directed = False)
 G.simplify(multiple=True, loops=True, combine_edges="max")
 
 print("\nPlotting KNN metrics...")
-knn_indices, knn_dists = knn_matrix2indist(knn_matrix)
+knn_indices, knn_dists = knn_matrix2indist(knn_matrix, opts["n_neighbors"])
 nnd = knn_dists[:,1] ; nnd = nnd[np.isfinite(nnd)]
-fnd = knn_dists[:,opts['n_neighbors']-1] ; fnd = fnd[np.isfinite(fnd)]
+fnd = knn_dists[:,-1] ; fnd = fnd[np.isfinite(fnd)]
 ies = np.array(G.degree(mode="in"))
-tlu = np.array(G.transitivity_local_undirected(mode="zero", weights="weight"))
+tlu = np.array(G.transitivity_local_undirected(mode="zero", weights=None))
 # con = np.array(G.constraint(weights="weight"))
 # nss = np.array(G.neighborhood_size(order=2))
 # sts = np.array(G.strength(weights="weight"))
@@ -142,7 +132,7 @@ if knn_filter:
 
     # Filter low clustering coefficient (<0.2)
     m = tlu>=0.2
-    knn_matrix = knn_mask.apply_mask(knn_matrix, m)
+    knn_matrix = knn_mask.apply_mask(knn_matrix, m, "KNN filter")
     G = G.subgraph(np.where(m)[0])
     print(f"{np.sum(~m)} beads removed")
     del m ; gc.collect()
@@ -151,10 +141,7 @@ if knn_filter:
     # Filter too-high or too-low in-edges?
     # New collision detector?
 
-# at this point, we have:
-#  final G (for leiden)
-#  final knn_matrix (for umap)
-#  final knn_mask (for sb)
+# At this point we have G, knn_matrix, and knn_mask
 
 ### Initialization #############################################################
 
@@ -164,36 +151,39 @@ def leiden_init(G):
     import leidenalg as la
 
     print("    Leiden cluster...")
-    # https://igraph.org/python/doc/api/igraph.Graph.html#community_leiden
-    #a1 = G.community_leiden(objective_function='modularity', weights=None, resolution=160, beta=0.01, n_iterations=2, node_weights=None)
-    # CPM, res=1 - 11.39
-    # mod, res=1 - 33.968
-    # CPM, res=160 - 11.588
-    # mod, res=160 - 32.728
-    # start_time = time.time()
-    # https://igraph.org/python/doc/api/igraph._igraph.GraphBase.html
-    # community_walktrap through community_edge_betweenness
-    # end_time = time.time()
-    # print(end_time - start_time)
-    # maintain cluster size, dynamic resolution?
+    # G.community_leiden(objective_function='modularity', weights=None, resolution=160, beta=0.01, n_iterations=2)
+    # community_fastgreedy
+    # community_infomap
+    # community_label_propagation
+    # community_leading_eigenvector
+    # community_leading_eigenvector_naive
+    # community_leiden
+    # community_multilevel
+    # community_optimal_modularity
+    # community_spinglass
+    # community_walktrap
     
     # https://leidenalg.readthedocs.io/en/stable/reference.html
-    partition = la.find_partition(G, la.RBConfigurationVertexPartition, resolution_parameter=160)
-    # leidenalg.find_partition(graph, partition_type, initial_membership=None, weights=None, n_iterations=2, max_comm_size=0, seed=None, **kwargs)
-
+    partition = la.find_partition(graph=G,
+                                  partition_type=la.RBConfigurationVertexPartition,
+                                  weights=None, # cosine?
+                                  n_iterations=2,
+                                  max_comm_size=1000,
+                                  resolution_parameter=160)
     membership = np.array(partition.membership)
     print(f'    Number of clusters: {len(np.unique(membership))}')
     print(f'    Modularity: {partition.modularity}')
 
-    # Create the Leiden CSR graph
+    # Contract KNN graph -> Leiden graph
     print("    Leiden graph...")
     G.contract_vertices(membership, combine_attrs=None)
     G.simplify(multiple=False, loops=True, combine_edges=None)
     G.es["weight"] = [1 - w for w in G.es["weight"]] # cosine distance -> similarity
     G.simplify(multiple=True, loops=True, combine_edges=sum)
+    # Convert Leiden graph to CSR
     i, j = zip(*G.get_edgelist())
     ic_edges = sp.csr_matrix((G.es["weight"], (i,j)), shape=(G.vcount(), G.vcount()))
-    ic_edges = ic_edges + ic_edges.T
+    ic_edges = ic_edges + ic_edges.T # ic_edges = ic_edges.maximum(ic_edges.T)
     ic_edges.setdiag(0)
     ic_edges.eliminate_zeros()
 
@@ -209,9 +199,11 @@ def leiden_init(G):
         C.data = np.minimum(C.data, 1)
         C.setdiag(0)
         C.eliminate_zeros()
-        return(knn_matrix2indist(C))
+        # assert isinstance(C, sp.csr_matrix) and np.all(np.diff(C.indptr) == 14)
+        return(C)
         
-    knn_indices, knn_dists = my_knn(ic_edges)
+    knn_indices, knn_dists = knn_matrix2indist(my_knn(ic_edges))
+    assert ic_edges.shape[0] == ic_edges.shape[1] == knn_indices.shape[0] == knn_dists.shape[0]
 
     print("    Leiden UMAP...")
     from umap import UMAP        
@@ -219,7 +211,8 @@ def leiden_init(G):
                    metric = "cosine",
                    random_state = None,
                    verbose = False,
-                   low_memory=True,
+                   n_neighbors=15,
+                   #min_dist=0.01,
                    n_epochs = 5000,
                    precomputed_knn = (knn_indices, knn_dists),
                    init = "spectral",
@@ -227,26 +220,63 @@ def leiden_init(G):
                   )
     embedding = reducer.fit_transform(ic_edges)
     embedding -= np.mean(embedding, axis=0) # center
+    return membership, ic_edges, embedding
 
-    return embedding, membership
+if os.path.isfile(os.path.join(out_dir, 'init.npz')):
+    # Loading previous
+    print("TODO")
+    membership, ic_edges, embedding = leiden_init(G)
+else:
+    membership, ic_edges, embedding = leiden_init(G)
 
-embedding, membership = leiden_init(G)
-init = embedding[membership]
-
-print("Saving Leiden initialization...")
-
+print("    Leiden plots...")
+# Metrics plot
+# umis, connections, etc - leiden filter
 # Initialization plot
 fig, ax = plt.subplots(1, 1, figsize=(8, 8))
 ax.scatter(embedding[:, 0], embedding[:, 1], s=1)
 ax.set_title('Leiden Initialization')
 ax.axis('equal')
-fig.savefig(os.path.join(out_dir, "leiden.pdf"), dpi=200)
+fig.savefig(os.path.join(out_dir, "init.pdf"), dpi=200)
 del fig, ax
 
-np.savez_compressed(os.path.join(out_dir, 'leiden.npz'), embedding=embedding, membership=membership)
-del G
+print("    Leiden initialization...")
+# Generate bead initialization - stacked
+'''
+init = embedding[membership]
+'''
+# Generate bead initialization - random jitter (default)
+'''
+init = embedding[membership]
+from scipy.spatial import KDTree
+sd = np.mean(KDTree(embedding).query(embedding, k=2)[0][:,1]) / 1000
+init += np.random.normal(loc=0, scale=sd, size=(init.shape[0],2))
+'''
+# Generate bead initialization - (weighted?) average
+knn_indices, knn_dists = knn_matrix2indist(knn_matrix, opts["n_neighbors"])
+init = embedding[membership]
+for _ in range(3):
+    init = np.stack((np.mean(init[knn_indices, 0], axis=1),
+                     np.mean(init[knn_indices, 1], axis=1)), axis=1)
+del knn_indices, knn_dists
+
+print("Saving Leiden initialization...")
+np.savez_compressed(os.path.join(out_dir, 'init.npz'), embedding=embedding, membership=membership, init=init)
+del G, knn_matrix ; gc.collect()
+
+# at this point, we have:
+#  init
+#  final knn_mask (for sb)
 
 ### Load + subset data #########################################################
+
+print(f"\nFiltering beads with <{opts['n_neighbors']} neighbors...")
+while any(np.diff(knn_matrix.indptr) < opts['n_neighbors'] - 1):
+    m = np.diff(knn_matrix.indptr) >= opts['n_neighbors'] - 1
+    print(f"{np.sum(~m)} beads removed")
+    knn_matrix = knn_mask.apply_mask(knn_matrix, m)
+    del m ; gc.collect()
+assert all(np.diff(knn_matrix.indptr) >= opts['n_neighbors'] - 1)
 
 # Load barcodes
 print('Loading the barcodes...')
@@ -309,7 +339,7 @@ fig.savefig(os.path.join(out_dir, "umap_umi.pdf"), dpi=200)
 
 # Make summary pdf
 # creation order: knn_filter, knn_graph, knn, leiden, umap, umaps, convergence, umap_umi
-names = ["umap", "convergence", "umaps", "umap_umi", "leiden", "knn"]
+names = ["umap", "convergence", "umaps", "umap_umi", "init", "knn"]
 paths = [os.path.join(out_dir, n+".pdf") for n in names]
 files = [p for p in paths if os.path.isfile(p)]
 if len(files) > 0:
