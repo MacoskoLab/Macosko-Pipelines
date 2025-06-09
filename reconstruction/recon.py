8b73349bb298026b88fadf72ec869ad0f304d2b5
MatthewShabet <matthew.shabet@gmail.com>
2025-06-09 17:54:13 -0400

    Update recon.py

--- reconstruction/recon.py
+++ reconstruction/recon.py
@@ -1,11 +1,8 @@
 import os
 import gc
-import sys
-import csv
 import argparse
 import numpy as np
 import pandas as pd
-import igraph as ig
 import scipy.sparse as sp
 from pypdf import PdfWriter
 from helpers import *
@@ -16,6 +13,7 @@ def get_args():
     parser.add_argument("-o", "--out_dir", help="output data folder", type=str, default=".")
     parser.add_argument("-c", "--cores", type=int, default=-1)
     parser.add_argument("-b", "--bead", type=int, default=2)
+    parser.add_argument("-D", "--diameter", type=float, default=0) #in microns, <= 0 estimates using bead count
     parser.add_argument("-K", "--knn_filter", help="KNN graph filter", type=bool, default=False)
     
     parser.add_argument("-nn", "--n_neighbors", type=int, default=30)
@@ -36,6 +34,7 @@ in_dir = args.in_dir             ; print(f"input directory: {in_dir}")
 out_dir = args.out_dir           ; print(f"output directory: {out_dir}")
 cores = args.cores               ; print(f"cores: {cores}")
 bead = args.bead                 ; print(f"bead: {bead}")
+diameter = args.diameter         ; print(f"diameter: {'auto' if diameter <= 0 else diameter}")
 knn_filter = args.knn_filter     ; print(f"KNN filter: {knn_filter}")
 
 opts = dict()
@@ -53,6 +52,7 @@ print("UMAP options:\n" + '\n'.join(f'    {k} = {v}' for k, v in opts.items()))
 name = f"UMAP{bead}_"
 name += "_".join([f"{''.join(s[0] for s in k.split('_'))}{v}" for k,v in opts.items()])
 name += ("_K" if knn_filter else "")
+name += (f"_D{diameter}" if diameter > 0 else "")
 print(f"name: {name}")
 
 # Check in/out directories
@@ -81,11 +81,9 @@ print("\nLoading data...")
 # Load KNN matrix
 knn_matrix = sp.load_npz(os.path.join(in_dir, f'knn{bead}.npz')).tocsr()
 knn_matrix.setdiag(0) ; knn_matrix.eliminate_zeros()
-assert np.diff(knn_matrix.indptr).max() >= opts["n_neighbors"] - 1
 
 # Load sb
 sb = pd.read_csv(os.path.join(in_dir, f"sb{bead}.txt.gz"), header=None, names=['sb'])['sb'].to_numpy()
-assert len(sb) == knn_matrix.shape[0] == knn_matrix.shape[1]
 
 # Load the number of UMIs per bead
 if os.path.isfile(os.path.join(in_dir, f"readumi_per_sb{bead}.csv.gz")):
@@ -105,29 +103,28 @@ else:
     del df
 
 # Compute scaling factor
-diameter_micron = estimate_diameter(in_dir)
-print(f"Estimated puck diameter: {diameter_micron}")
+if diameter <= 0:
+    diameter_micron = estimate_diameter(in_dir)
+    print(f"Estimated puck diameter: {diameter_micron}")
+else:
+    diameter_micron = diameter
 
-knn = KNN(knn_matrix, sb, umi)
+# Create KNN object
+knn = KNN(knn_matrix, sb, umi, opts["n_neighbors"])
 del knn_matrix, sb, umi ; gc.collect()
 
-print("\nFiltering beads with no neighbors...")
-knn.apply_mask(np.diff(knn.matrix.indptr) != 0, "No neighbors")
+# Initial bead filter
+knn.filter_toofewneighbors()
+knn.filter_disconnected()
 
 ### Load KNN graph ############################################################
 
 print("\nCreating KNN graph...")
-knn_matrix_k = csr_k_nearest(knn.matrix, k=opts['n_neighbors']-1)
-G = ig.Graph(n = knn_matrix_k.shape[0],
-             edges = zip(*knn_matrix_k.nonzero()),
-             edge_attrs = {'weight': 1-knn_matrix_k.data}, # cosine similarity weights
-             directed = False)
-G.simplify(multiple=True, loops=True, combine_edges="max")
-del knn_matrix_k ; gc.collect()
+G = knn.graph()
 
 print("\nPlotting KNN metrics...")
-knn_indices, knn_dists = knn_matrix2indist(knn.matrix, k=opts["n_neighbors"])
-nnd = knn_dists[:,1] ; nnd = nnd[np.isfinite(nnd)]
+knn_indices, knn_dists = knn.inddist()
+nnd = knn_dists[:,0] ; nnd = nnd[np.isfinite(nnd)]
 fnd = knn_dists[:,-1] ; fnd = fnd[np.isfinite(fnd)]
 ies = np.array(G.degree(mode="in"))
 tlu = np.array(G.transitivity_local_undirected(mode="zero", weights='weight'))
@@ -169,150 +166,34 @@ if knn_filter:
     # Filter too-high or too-low in-edges?
     # New collision detector?
 
-### Compute Initialization #####################################################
+# Intermediate bead filter
+knn.filter_toofewneighbors()
+knn.filter_disconnected()
 
-def leiden_init(G):
-    import leidenalg as la
-
-    print("    Leiden cluster...")
-    # https://igraph.org/python/api/0.9.7/igraph._igraph.GraphBase.html#community_edge_betweenness
-    # G.community_leiden(objective_function='modularity', weights=None, resolution=160, beta=0.01, n_iterations=2)
-    # G.community_edge_betweenness()
-    # G.community_fastgreedy
-    # G.community_infomap
-    # G.community_label_propagation
-    # G.community_leading_eigenvector
-    # G.community_leading_eigenvector_naive
-    # G.community_leiden
-    # G.community_multilevel
-    # G.community_optimal_modularity
-    # G.community_spinglass
-    # G.community_walktrap
-    
-    # https://leidenalg.readthedocs.io/en/stable/reference.html
-    partition = la.find_partition(graph=G,
-                                  partition_type=la.RBConfigurationVertexPartition,
-                                  weights='weight',
-                                  n_iterations=2,
-                                  #max_comm_size=1000,
-                                  resolution_parameter=160)
-    membership = np.array(partition.membership)
-    print(f'    Number of clusters: {len(np.unique(membership))}')
-    print(f'    Modularity: {partition.modularity}')
-
-    # Contract KNN graph -> Leiden graph
-    print("    Leiden graph...")
-    G.contract_vertices(membership, combine_attrs=None)
-    G.simplify(multiple=True, loops=True, combine_edges=sum)
-    
-    # Convert Leiden graph to symmetric CSR
-    i, j = zip(*G.get_edgelist())
-    ic_edges = sp.csr_matrix((G.es["weight"], (i,j)), shape=(G.vcount(), G.vcount()))
-    ic_edges = ic_edges.maximum(ic_edges.T)
-    assert ic_edges.has_canonical_format
-    assert np.all(ic_edges.diagonal() == 0)
-    ic_edges.eliminate_zeros()
-
-    # Create the Leiden KNN (for UMAP)
-    print("    Leiden KNN...")
-    def my_knn(mat):
-        from sklearn.preprocessing import normalize
-        from sparse_dot_topn import sp_matmul_topn
-        mat_norm = normalize(mat, norm='l2', axis=1, copy=True)
-        C = sp_matmul_topn(mat_norm, mat_norm.T, top_n=15, n_threads=1)
-        C.data = 1 - C.data # cosine similarity -> distance
-        C.data = np.maximum(C.data, 0)
-        C.data = np.minimum(C.data, 1)
-        C.setdiag(0)
-        C.eliminate_zeros()
-        # assert isinstance(C, sp.csr_matrix) and np.all(np.diff(C.indptr) == 14)
-        return(C)
-        
-    knn_indices, knn_dists = knn_matrix2indist(my_knn(ic_edges))
-    assert ic_edges.shape[0] == ic_edges.shape[1] == knn_indices.shape[0] == knn_dists.shape[0]
-
-    print("    Leiden UMAP...")
-    from umap import UMAP        
-    reducer = UMAP(n_components = 2,
-                   metric = "cosine",
-                   random_state = None,
-                   verbose = False,
-                   n_neighbors=15,
-                   #min_dist=0.01,
-                   n_epochs = 5000,
-                   precomputed_knn = (knn_indices, knn_dists),
-                   init = "spectral",
-                   n_jobs = -1
-                  )
-    embedding = reducer.fit_transform(ic_edges)
-    embedding -= np.mean(embedding, axis=0)
-    return membership, ic_edges, embedding
+### Leiden Initialization ######################################################
 
 print("\nGenerating Leiden embedding...")
 assert G.vcount() == knn.matrix.shape[0] == knn.matrix.shape[1]
 membership, ic_edges, embedding = leiden_init(G)
 assert G.vcount() == embedding.shape[0] == np.unique(membership).shape[0]
-assert membership.shape[0] == knn.matrix.shape[0] == knn.matrix.shape[1]
-del G ; gc.collect()
+assert len(knn.membership) == len(membership)
+knn.membership = membership
+del G, membership ; gc.collect()
 
-### Plot Initialization ########################################################
-
-print("\nLeiden plots...")
-fig, ax = plt.subplots(1, 1, figsize=(8, 8))
-ax.scatter(x=embedding[:, 0], y=embedding[:, 1], s=1)
-ax.set_title('Leiden Initialization')
-ax.axis('equal')
-fig.savefig(os.path.join(out_dir, "init.pdf"), dpi=200)
-del fig, ax
-
-fig, axs = plt.subplots(3, 2, figsize=(8, 8))
-def my_plot(embedding, vec, lab, axs):
-    axs[0].hist(vec, bins=30, color='skyblue', edgecolor='black')
-    axs[0].set_title(lab)
-
-    axs[1].scatter(x=embedding[:,0], y=embedding[:,1], c=vec, cmap='viridis', s=1)
-    axs[1].set(xticks=[], yticks=[], xticklabels=[], yticklabels=[])
-    [spine.set_visible(False) for spine in axs[1].spines.values()]
-    axs[1].set_aspect('equal')
-    axs[1].set_title(lab)
-
-vec_beads = np.bincount(membership)
-vec_strength = np.sum(ic_edges, axis=0).A1
-vec_logumi = np.log10(np.bincount(membership, weights=knn.umi))
-
-my_plot(embedding, vec_beads, 'Beads', axs[0])
-my_plot(embedding, vec_strength, 'Strength', axs[1])
-my_plot(embedding, vec_logumi, 'log10(UMI)', axs[2])
-
-plt.tight_layout()
-fig.savefig(os.path.join(out_dir, "init2.pdf"), dpi=200)
-del fig, axs, my_plot, ic_edges
-
-### Process Initialization #####################################################
+# Plot Leiden initialization
+leiden_plots(embedding, knn, ic_edges, out_dir)
+del ic_edges ; gc.collect()
 
 print("\nLeiden filter...")
-# TODO (make sure to update both knn and membership)
+# TODO
 
-print(f"\nFiltering beads with <{opts['n_neighbors']} neighbors...")
-while any(np.diff(knn.matrix.indptr) < opts['n_neighbors']):
-    m = np.diff(knn.matrix.indptr) >= opts['n_neighbors']
-    knn.apply_mask(m, "Too few neighbors")
-    membership = membership[m]
-    del m ; gc.collect()
-
-print("\nRemoving disconnected components...")
-knn_matrix_k = csr_k_nearest(knn.matrix, k=opts['n_neighbors']-1)
-n_components, labels = sp.csgraph.connected_components(knn_matrix_k, directed=False, return_labels=True)
-m = labels == np.bincount(labels).argmax()
-knn.apply_mask(m, "Disconnected")
-membership = membership[m]
-del m, n_components, labels, knn_matrix_k ; gc.collect()
+# Final bead filter
+knn.filter_toofewneighbors()
+knn.filter_disconnected()
 
 # NO MORE FILTERING PAST THIS POINT!
 
-assert knn.matrix.shape[0] == knn.matrix.shape[1] == membership.shape[0]
-assert np.unique(membership).shape[0] <= embedding.shape[0] and np.max(membership) < embedding.shape[0]
-
+# Plot filtering results
 fig, ax = plt.subplots(figsize=(8, 8))
 top_lines = [in_dir, name, f"Estimated diameter: {diameter_micron}µm"]
 bot_lines = ["<Beads filtered>"] + [f"{n}: {b}" for n,b in knn.history]
@@ -324,28 +205,28 @@ fig.savefig(os.path.join(out_dir, "name.pdf"))
 print("\nInitializing bead coordinates...")
 # Generate bead initialization - stacked
 '''
-init = embedding[membership]
+init = embedding[knn.membership]
 '''
 # Generate bead initialization - random jitter (default)
 '''
-init = embedding[membership]
+init = embedding[knn.membership]
 from scipy.spatial import KDTree
 sd = np.mean(KDTree(embedding).query(embedding, k=2)[0][:,1]) / 1000
 init += np.random.normal(loc=0, scale=sd, size=(init.shape[0],2))
 '''
 # Generate bead initialization - weighted average
-knn_indices, knn_dists = knn_matrix2indist(knn.matrix, opts["n_neighbors"])
+knn_indices, knn_dists = knn.inddist()
 weights = 1-knn_dists ; weights[knn_indices < 0] = 0
-init = embedding[membership]
+init = embedding[knn.membership]
 for _ in range(3):
     init = np.stack((np.average(init[knn_indices, 0], axis=1, weights=weights),
                      np.average(init[knn_indices, 1], axis=1, weights=weights)), axis=1)
 del knn_indices, knn_dists, weights
 
 print("\nSaving initialization...")
-np.savez_compressed(os.path.join(out_dir, 'init.npz'), embedding=embedding, membership=membership, init=init)
+np.savez_compressed(os.path.join(out_dir, 'init.npz'), embedding=embedding, membership=knn.membership, init=init)
 assert init.shape[0] == knn.matrix.shape[0] == knn.matrix.shape[1]
-del membership, embedding ; gc.collect()
+del embedding ; gc.collect()
 
 ### UMAP TIME ##################################################################
 
@@ -355,7 +236,6 @@ reducer = UMAP(n_components = 2,
                metric = "precomputed",
                random_state = None,
                verbose = True,
-               low_memory=True,
                init = init,
                n_jobs = cores,
                **opts
@@ -363,12 +243,13 @@ reducer = UMAP(n_components = 2,
 embedding = reducer.fit_transform(knn.matrix.maximum(knn.matrix.T))
 embedding -= np.mean(embedding, axis=0)
 assert np.all(np.isfinite(embedding))
-assert embedding.shape[0] == knn.sb.shape[0] == knn.umi.shape[0] == knn.matrix.shape[0] == knn.matrix.shape[1]
 
 ### WRITE RESULTS ##############################################################
 
 print("\nWriting results...")
-knn_indices, knn_dists = knn_matrix2indist(knn.matrix, opts["n_neighbors"])
+knn.check()
+assert embedding.shape[0] == knn.matrix.shape[0]
+knn_indices, knn_dists = knn.inddist()
 
 # Create the Puck file
 embedding_scaled = embedding / np.mean(np.ptp(embedding, axis=0)) * diameter_micron
