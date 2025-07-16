24fff542a216989ad273043272a4ccfe5f8a0162
MatthewShabet <matthew.shabet@gmail.com>
2025-07-16 16:29:59 -0400

    Bug fix

--- workflows/terra/README.md
+++ workflows/terra/README.md
@@ -5,6 +5,16 @@ Helpful links
 
 Helpful commands
 ================
+gcloud commands
+---------------
+```
+gcloud auth application-default login --scopes 'openid,https://www.googleapis.com/auth/userinfo.email,https://www.googleapis.com/auth/cloud-platform,https://www.googleapis.com/auth/sqlservice.login,https://www.googleapis.com/auth/drive,https://www.googleapis.com/auth/spreadsheets'
+```
+
+```
+gcloud auth application-default set-quota-project velina-208320
+```
+
 .h5 commands
 ------------
 * h5dump -n SBcounts.h5: list all contents
--- workflows/terra/submit.py
+++ workflows/terra/submit.py
@@ -1,9 +1,16 @@
 import sys
+import gspread
 import argparse
 import pandas as pd
 import firecloud.api as fapi
+from google.auth import default
 from google.cloud import storage
-BUCKET = "fc-secure-d99fbd65-eb27-4989-95b4-4cf559aa7d36"
+from gspread_dataframe import get_as_dataframe
+
+# TODO: caps
+wnamespace = "testmybroad"
+workspace = "Macosko-Pipelines"
+cnamespace = "macosko-pipelines"
 
 def get_args():
     parser = argparse.ArgumentParser(description='Terra job submission script')
@@ -13,105 +20,47 @@ def get_args():
     parser.add_argument("--dryrun", action='store_true')
     args = parser.parse_args()
     return args
+
 args = get_args()
 workflow = args.workflow             ; print(f"workflow: {workflow}")
 bcl = args.bcl.strip("/ \t\n\r")     ; print(f"     bcl: {bcl}")
 index = args.index.strip("/ \t\n\r") ; print(f"   index: {index}")
 dryrun = args.dryrun                 ; print(f"  dryrun: {dryrun}")
 
+assert workflow in ["cellranger-count", "slide-tags", "recon", "reconstruction"]
 assert not any(c.isspace() for c in bcl), f"remove whitespace from bcl ({bcl})"
 assert not any(c.isspace() for c in index), f"remove whitespace from index ({index})"
 
-# cache=TRUE
-
-# GCP helpers:
+# Load bucket
+BUCKET = "fc-secure-d99fbd65-eb27-4989-95b4-4cf559aa7d36"
 bucket = storage.Client().bucket(BUCKET)
-bucket.reload() # Check bucket access
-
-# Trim path if entire URL is specified
-# def trim_path(path):
-#     if path.startswith("gs://"):
-#         path = path[len("gs://"):]
-#     if path.startswith(BUCKET):
-#         path = path[len(BUCKET):]
-#     if path.startswith("/"):
-#         path = path[len("/"):]
-#     return path
-
-# Check if a file exists in the bucket
-# def file_exists(path):
-#     blob = bucket.blob(trim_path(path))
-#     return blob.exists()
-
-# Check if a directory exists in the bucket
-# def dir_exists(path):
-#     if not path.endswith("/"):
-#         path += "/"
-    
-#     blobs = bucket.list_blobs(prefix=trim_path(path), delimiter="/", max_results=1)
-#     return any(True for _ in blobs)
-
-'''
-# The internal DNS within a terra instance redirects googleapis.com -> restricted.google.com and bans the request
-# Don't mess with /etc/resolv.conf, it seems to have side effects
-# Modify the python to use dns_server=8.8.8.8 without affecting the rest (hopefully)
-import dns.resolver
-import socket
-def custom_getaddrinfo(host, port, family=0, type=0, proto=0, flags=0, dns_server='8.8.8.8'):
-    resolver = dns.resolver.Resolver()
-    resolver.nameservers = [dns_server]
-    try:
-        answer_ipv4 = resolver.resolve(host, 'A')
-    except (dns.resolver.NoAnswer, dns.resolver.NXDOMAIN, dns.resolver.Timeout):
-        answer_ipv4 = []
-    try:
-        answer_ipv6 = resolver.resolve(host, 'AAAA')
-    except (dns.resolver.NoAnswer, dns.resolver.NXDOMAIN, dns.resolver.Timeout):
-        answer_ipv6 = []
-    if not answer_ipv4 and not answer_ipv6:
-        raise socket.gaierror(socket.EAI_NONAME, 'Name or service not known')
-    addresses = [(socket.AF_INET, a.to_text()) for a in answer_ipv4] + [(socket.AF_INET6, a.to_text()) for a in answer_ipv6]
-    results = []
-    for af, addr in addresses:
-        if family != 0 and family != af:
-            continue
-        if type == socket.SOCK_STREAM:
-            proto = socket.IPPROTO_TCP
-        elif type == socket.SOCK_DGRAM:
-            proto = socket.IPPROTO_UDP
-        else:
-            raise ValueError("Unknown socket type")
-        results.append((af, type, proto, '', (addr, port)))
-    if not results:
-        raise socket.gaierror(socket.EAI_FAMILY, 'Address family not supported')
-    return results
-# Replace the default getaddrinfo with custom_getaddrinfo
-socket.getaddrinfo = custom_getaddrinfo
-'''
+bucket.reload() 
 
 '''
 workflow="cellranger-count"
 bcl="250527_SL-EXB_0558_B22Y57YLT4"
 index="all"
+dryrun=False
 
 workflow="recon"
 bcl="250521_SL-EXA_0515_A22YGT5LT4"
 index="all"
+dryrun=False
 
 workflow="slide-tags"
 bcl="250527_SL-EXB_0558_B22Y57YLT4"
 index="all"
+dryrun=False
 '''
 
-# Load the worksheet
+# Load the worksheet, select the columns
+sh = gspread.authorize(default()[0]).open_by_key("1NOaWXARQiSA6fquOtcouQPREPN4buYIf13tq_F6D9As")
 if workflow in ["cellranger-count", "slide-tags"]:
-    df = pd.read_csv('https://docs.google.com/spreadsheets/d/1NOaWXARQiSA6fquOtcouQPREPN4buYIf13tq_F6D9As/export?format=csv&gid=0')
+    df = get_as_dataframe(sh.worksheet("Slide-tags"))
     cols = ["BCL", "Reference", "RNAIndex", "SBIndex", "Puck"]
 elif workflow in ["recon", "reconstruction"]:
-    df = pd.read_csv('https://docs.google.com/spreadsheets/d/1NOaWXARQiSA6fquOtcouQPREPN4buYIf13tq_F6D9As/export?format=csv&gid=229934426')
-    cols = ["BCL", "Index", "bc1", "bc2"]
-else:
-    sys.exit(f"Unknown workflow: {workflow}")
+    df = get_as_dataframe(sh.worksheet("Recon"))
+    cols = ["BCL", "Index", "bc1", "bc2", "params"]
 
 
 # Clean the worksheet
@@ -131,8 +80,6 @@ if workflow in ["cellranger-count", "slide-tags"]:
     idx_col = "RNAIndex"
 elif workflow in ["recon", "reconstruction"]:
     idx_col = "Index"
-else:
-    sys.exit(f"Unknown workflow: {workflow}")
     
 def assert_unique_column(series):
     assert series.notna().all(), f"Column has NA values:\n{series}"
@@ -140,7 +87,7 @@ def assert_unique_column(series):
     assert series.is_unique, f"Column has repeated values:\n{series}"
 
 assert_unique_column(df[idx_col])
-df = df if index == "all" else df[df[idx_col] == index]
+df = df if index.lower() == "all" else df[df[idx_col] == index]
 print(f"Index rows found: {len(df.index)}")
 assert len(df.index) >= 1, f"No index rows found ({index})"
 
@@ -158,21 +105,15 @@ if workflow == "cellranger-count":
     assert df["Reference"].isin(refs).all(), f"Reference {set(df['Reference'])-refs} does not exist in the bucket"
 
     # Assert the output gene-expression folder does not exist
-    assert_unique_column(df["RNAIndex"])
     count_blobs = bucket.list_blobs(prefix=f"gene-expression/{bcl}")
     counts = {blob.name.split("/")[2] for blob in count_blobs}
     assert not df["RNAIndex"].isin(counts).any(), f"Output {set(df['RNAIndex'])&counts} already exists in the bucket"
 
 elif workflow == "slide-tags":
     # Assert RNA input exists
-    assert_unique_column(df["RNAIndex"])
     count_blobs = bucket.list_blobs(prefix=f"gene-expression/{bcl}")
     counts = {blob.name.split("/")[2] for blob in count_blobs}
     assert df["RNAIndex"].isin(counts).all(), f"GEX for {set(df['RNAIndex'])-counts} does not exist in the bucket"
-
-    # Assert SB input exists
-    assert_full_column(df["SBIndex"])
-    bucket.list_blobs(prefix=f"fastqs/{bcl}")
 
     # Assert puck file exists and is unique
     assert_full_column(df["Puck"])
@@ -191,55 +132,50 @@ elif workflow == "slide-tags":
     pucks = apply.pucks_to_URIs(df["Puck"])
     assert False
 
-elif workflow in ["recon", "reconstruction"]:
-    assert_unique_column(df["Index"])
-else:
-    sys.exit(f"Unknown workflow: {workflow}")
-
 
 # Compute memory requirements
-def compute_fastq_sizes(series):
-    assert_full_column(series)
-    fastq_blobs = bucket.list_blobs(prefix=f"fastqs/{bcl}")
-    fastqs = [(blob.name, blob.size) for blob in fastq_blobs if blob.name.endswith(".fastq.gz")]
-    mem_GBs = [int(sum(s for n,s in fastqs if "/"+i+"_S" in n) / 1e9) for i in series]
-    return mem_GBs
-    
+fastq_blobs = bucket.list_blobs(prefix=f"fastqs/{bcl}")
+fastqs = [(blob.name, blob.size) for blob in fastq_blobs if blob.name.endswith(".fastq.gz")]
+getfastqsizes = lambda inds: [int(sum(s for n,s in fastqs if "/"+i+"_S" in n) / 1e9) for i in inds]
+
 if workflow == "cellranger-count":
     # Compute the FASTQ sizes
-    mem_GBs = compute_fastq_sizes(df["RNAIndex"])
-    mem_GBs = [1.5*mem for mem in mem_GBs]
+    mem_GBs = getfastqsizes(df["RNAIndex"])
+    mem_GBs = [int(5*mem+20) for mem in mem_GBs]
 
 elif workflow == "slide-tags":
+    # Compute the FASTQ sizes
+    mem_GBs_fastq = getfastqsizes(df["SBIndex"])
+    mem_GBs_fastq = [int(2*mem) for mem in mem_GBs_fastq]
+
+    # Compute the SBcounts.h5 size
+    tags_blobs = bucket.list_blobs(prefix=f"slide-tags/{bcl}")
+    tags = [(blob.name, blob.size) for blob in tags_blobs if blob.name.endswith("/SBcounts.h5")]
+
     assert False
     
 elif workflow in ["recon", "reconstruction"]:
     # Compte the FASTQ sizes
-    assert_full_column(df["Index"])
-    mem_GBs_fastq = compute_fastq_sizes(df["Index"])
+    mem_GBs_fastq = getfastqsizes(df["Index"])
     mem_GBs_fastq = [int(2*mem) for mem in mem_GBs_fastq]
     
     # Compute the intermediate matrix sizes
     recon_blobs = bucket.list_blobs(prefix=f"recon/{bcl}")
-    recons = [(blob.name, blob.size) for blob in recon_blobs if blob.name.endswith("knn2.npz")]
+    recons = [(blob.name, blob.size) for blob in recon_blobs if blob.name.endswith("/knn2.npz")]
     mem_GBs_mat = [max((s for n,s in recons if "/"+i+"/" in n), default=0) / 1e9 for i in df["Index"]]
     mem_GBs_mat = [int(25*mem) for mem in mem_GBs_mat]
 
-    # Take the minimum available
-    mem_GBs = [min(x,y) if min(x,y) > 0 else max(x,y) for x,y in zip(mem_GBs_fastq, mem_GBs_mat)]
-    
-else:
-    sys.exit(f"Unknown workflow: {workflow}")
+    # Take the max (TODO)
+    mem_GBs = [max(x,y) for x,y in zip(mem_GBs_fastq, mem_GBs_mat)]
 
 assert all(m > 0 for m in mem_GBs), f"Incomplete memory estimation: {mem_GBs} (missing input files)"
+mem_GBs = [int(max(mem, 64)) for mem in mem_GBs]
 print(f"Memory (GB): {mem_GBs}")
 
 
 # Compute the names, assert the jobs are not already running
 job_names = ["_".join([workflow, idx, bcl]) for idx in df[idx_col]]
-assert len(job_names) == len(set(job_names))
-
-subs = fapi.list_submissions("testmybroad", "Macosko-Pipelines").json()
+subs = fapi.list_submissions(wnamespace, workspace).json()
 subs = [sub for sub in subs if sub["status"] not in ["Done","Aborted"]]
 running_job_names = [sub["userComment"] for sub in subs if "userComment" in sub]
 assert not set(job_names) & set(running_job_names), "Jobs already running!"
@@ -250,42 +186,12 @@ if dryrun:
     print(df)
     print("Dry run complete, no errors found - exiting...")
     sys.exit(0)
+# TODO: recon won't error if missing fastqs when reselecting barcodes
+# TODO: cached bc1/bc2 will likely differ from the sheet
+# TODO: check data types
 
 ### Terra Submission ###########################################################
 
-wnamespace = "testmybroad"
-workspace = "Macosko-Pipelines"
-cnamespace = "macosko-pipelines"
-
-def run_cellranger_count(bcl, index, reference, mem_GB, disk_GB, params="", user_comment=""):
-    body = fapi.get_workspace_config(wnamespace, workspace, cnamespace, "cellranger-count").json()
-    body["inputs"]["cellranger_count.bcl"] = f'"{bcl}"'
-    body["inputs"]["cellranger_count.index"] = f'"{index}"'
-    body["inputs"]["cellranger_count.reference"] = f'"{reference}"'
-    body["inputs"]["cellranger_count.mem_GB"] = f'"{mem_GB}"'
-    body["inputs"]["cellranger_count.disk_GB"] = f'"{disk_GB}"'
-    body["inputs"]["cellranger_count.params"] = f'"{params}"'
-    body["inputs"]["cellranger_count.docker"] = f''
-    res = fapi.update_workspace_config(wnamespace, workspace, cnamespace, "cellranger-count", body)
-    assert res.status_code == 200, res.json()['message']
-    
-    submit("cellranger-count", user_comment)
-    return True
-
-def run_reconstruction(bcl, index, mem_GB, disk_GB, params="", user_comment=""):
-    body = fapi.get_workspace_config(wnamespace, workspace, cnamespace, "reconstruction").json()  
-    body["inputs"]["reconstruction.bcl"] = f'"{bcl}"'
-    body["inputs"]["reconstruction.index"] = f'"{index}"'
-    body["inputs"]["reconstruction.mem_GB"] = f'"{mem_GB}"'
-    body["inputs"]["reconstruction.disk_GB"] = f'"{disk_GB}"'
-    #body["inputs"]["reconstruction.params"] = f'"{params}"'
-    #body["inputs"]["reconstruction.docker"] = f''
-    res = fapi.update_workspace_config(wnamespace, workspace, cnamespace, "reconstruction", body)
-    assert res.status_code == 200, res.json()['message']
-    
-    submit("reconstruction", user_comment)
-    return True
-    
 def submit(config, user_comment=""):
     # Validate the configuration
     res = fapi.validate_config(wnamespace, workspace, cnamespace, config).json()
@@ -295,25 +201,57 @@ def submit(config, user_comment=""):
     assert res["missingInputs"] == [], f"ERROR: missing input: \n{res['missingInputs']}"
     
     # Submit the job
-    res = fapi.create_submission(wnamespace, workspace, cnamespace, config, user_comment=user_comment)
+    res = fapi.create_submission(wnamespace, workspace, cnamespace, config, user_comment=user_comment, use_callcache=False)
     assert res.status_code == 201, res.status_code + ": " + res.json()['message']
     print(f"Submitted {config} {user_comment}")
 
+def run_cellranger_count(bcl, index, reference, mem_GB, disk_GB, params=None, user_comment=""):
+    body = fapi.get_workspace_config(wnamespace, workspace, cnamespace, "cellranger-count").json()
+    body["inputs"]["cellranger_count.bcl"] = f'"{bcl}"'
+    body["inputs"]["cellranger_count.index"] = f'"{index}"'
+    body["inputs"]["cellranger_count.reference"] = f'"{reference}"'
+    body["inputs"]["cellranger_count.mem_GB"] = f'{mem_GB}'
+    body["inputs"]["cellranger_count.disk_GB"] = f'{disk_GB}'
+    body["inputs"]["cellranger_count.params"] = f'"{params}"' if pd.notna(params) else f''
+    body["inputs"]["cellranger_count.docker"] = f''
+    res = fapi.update_workspace_config(wnamespace, workspace, cnamespace, "cellranger-count", body)
+    assert res.status_code == 200, res.json()['message']
+    
+    submit("cellranger-count", user_comment)
+    return True
+
+def run_reconstruction(bcl, index, mem_GB, disk_GB, bc1=None, bc2=None, lanes=None, params=None, user_comment=""):
+    body = fapi.get_workspace_config(wnamespace, workspace, cnamespace, "reconstruction").json()  
+    body["inputs"]["reconstruction.bcl"] = f'"{bcl}"'
+    body["inputs"]["reconstruction.index"] = f'"{index}"'
+    body["inputs"]["reconstruction.mem_GB"] = f'{mem_GB}'
+    body["inputs"]["reconstruction.disk_GB"] = f'{disk_GB}'
+    body["inputs"]["reconstruction.bc1"] = f'{bc1}' if pd.notna(bc1) else f''
+    body["inputs"]["reconstruction.bc2"] = f'{bc2}' if pd.notna(bc2) else f''
+    body["inputs"]["reconstruction.lanes"] = f'{lanes}' if pd.notna(lanes) else f''
+    body["inputs"]["reconstruction.params"] = f'"{params}"' if pd.notna(params) else f''
+    body["inputs"]["reconstruction.docker"] = f''
+    res = fapi.update_workspace_config(wnamespace, workspace, cnamespace, "reconstruction", body)
+    assert res.status_code == 200, res.json()['message']
+    
+    submit("reconstruction", user_comment)
+    return True
+    
 
 if workflow == "cellranger-count":
-    pass
+    assert False
 elif workflow == "slide-tags":
-    pass
+    assert False
 elif workflow in ["recon", "reconstruction"]:
-    for row, mem, job in zip(df.itertuples(index=False), mem_GBs, job_names):
-        run_reconstruction(row.BCL, row.Index, mem, mem, params="", user_comment=job)
-else:
-    sys.exit(f"Unknown workflow: {workflow}")
+    for r, m, j in zip(df.itertuples(index=False), mem_GBs, job_names):
+        assert r.Index.count("-") <= 1
+        idx, _, lanes = r.Index.partition("-") ; 
+        run_reconstruction(r.BCL, idx, m, m, r.bc1, r.bc2, lanes or None, r.params, j)
 
-### Terra Submission ###########################################################
+### Terra Commands #############################################################
 
 # List all submissions
-subs = fapi.list_submissions("testmybroad", "Macosko-Pipelines").json()
+subs = fapi.list_submissions(wnamespace, workspace).json()
 subs = [sub for sub in subs if sub["status"] not in ["Done","Aborted"]]
 print(f"Currently running submissions: {len(subs)}")
 
